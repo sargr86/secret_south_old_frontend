7e222d035b48a126f90f00241bdf0edc7b7f0591
Sofia Bruno <sofiabruno19@yahoo.com>
2019-03-22 16:48:11 +0400

    Added confirmation dialog, moved ferries service to services folder

--- src/app/admin/services/ferry.service.spec.ts
+++ src/app/admin/services/ferry.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FerryService } from './ferry.service';
+import {FerryService} from './ferry.service';
 
 describe('FerryService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
