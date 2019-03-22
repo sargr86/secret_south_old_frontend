95cf884a07435685ad790eaf28efd871b2e77c89
Grigor Sargsyan <sargr86@gmail.com>
2019-03-22 17:12:19 +0400

    Fixed mat table pagination for the listed items

--- src/app/admin/all-tours/all-tours.component.ts
+++ src/app/admin/all-tours/all-tours.component.ts
@@ -65,6 +65,7 @@ export class AllToursComponent implements OnInit {
             this.dataSource = new MatTableDataSource(this.users);
             this.dataLoading = false;
             this.dataSource.sort = this.sort;
+            this.dataSource.paginator = this.paginator;
 
             this.dataSource.sortingDataAccessor = (data: any, sortHeaderId: string): string => {
                 if (typeof data[sortHeaderId] === 'string') {
