3c4c702f6a0ca151f4f1e86d7c39fe328d3eac83
Sofia Bruno <sofiabruno19@yahoo.com>
2019-03-29 17:55:00 +0400

    Added toastr messages, form validation for save-ferry/tour

--- src/app/admin/tours/save-tour/save-tour.component.ts
+++ src/app/admin/tours/save-tour/save-tour.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ToursService} from '../../services/tours.service';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
+import {MapsAPILoader} from '@agm/core';
+import {SPINNER_DIAMETER} from '../../../shared/constants/settings';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
     selector: 'app-save-tour',
@@ -10,83 +13,159 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class SaveTourComponent implements OnInit {
 
+
+    @ViewChild('searchAddress')
+    public searchElementRef: ElementRef;
+
     partners;
     tourType;
     saveTourForm: FormGroup;
-    editCase = false;
     uploadImages;
     tourFields = {
-        'name': '',
-        'lat': '',
-        'lng': '',
-        'address': '',
-        'tours_type_id': '',
-        'partner_id': '',
+        'name': ['', Validators.required],
+        'lat': ['', Validators.required],
+        'lng': ['', Validators.required],
+        'address': ['', Validators.required],
+        'tours_type_id': ['', Validators.required],
+        'partner_id': ['', Validators.required]
     };
+    editCase = false;
+    dataLoading = false;
+    formProcessing = false;
+    spinnerDiameter = SPINNER_DIAMETER;
 
     constructor(
         private _tours: ToursService,
         private _fb: FormBuilder,
         public router: Router,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private mapsAPILoader: MapsAPILoader,
+        private toastr: ToastrService
     ) {
         this.getPartners();
         this.getToursType();
+
+
     }
 
     ngOnInit() {
         this.saveTourForm = this._fb.group(this.tourFields);
 
-        const id = this.route.snapshot.paramMap.get('id');
-        if (id) {
-            this._tours.getOneTour({id: id}).subscribe((dt: any) => {
-                this.tourFields['id'] = '';
-                this.saveTourForm = this._fb.group(this.tourFields);
-                this.saveTourForm.patchValue(dt[0]);
-                this.editCase = true;
-            });
-        }
+        this.mapsAPILoader.load().then(() => {
+            if (this.searchElementRef) {
+                const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {types: ['geocode']});
+                const id = this.route.snapshot.paramMap.get('id');
+                if (id) {
+                    this.dataLoading = true;
+                    this._tours.getOneTour({id: id}).subscribe((dt: any) => {
+                        if (dt && dt.length > 0) {
+
+                            this.tourFields['id'] = '';
+                            this.saveTourForm = this._fb.group(this.tourFields);
+                            this.saveTourForm.patchValue(dt[0]);
+                            this.editCase = true;
+                        }
+                        this.dataLoading = false;
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    /**
+     * Resets address and reloads maps api to allow user to select from drop down again
+     */
+    resetAddress() {
+        this.saveTourForm.patchValue({'address': ''});
+        this.mapsAPILoader.load().then(() => {
+            const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {types: ['geocode']});
+        });
     }
 
 
+    /**
+     * Gets partners list
+     */
     getPartners() {
         this._tours.getAllpartner().subscribe((r: any) => {
             this.partners = r['result'];
         });
     }
 
+    /**
+     * Gets tour types list
+     */
     getToursType() {
         this._tours.getAllTourType().subscribe((r: any) => {
             this.tourType = r['result'];
         });
     }
 
+    /**
+     * Gets uploaded file list
+     * @param files files object
+     */
     getFiles(files) {
         this.uploadImages = files.item(0);
     }
 
-    saveTour() {
-        const data = this.saveTourForm.value;
-        const fd = new FormData();
-        fd.append('lat', data.lat);
-        fd.append('lng', data.lng);
-        fd.append('name', data.name);
-        fd.append('tours_type_id', data.tours_type_id);
-        fd.append('partner_id', data.partner_id);
-        fd.append('address', data.address);
-        fd.append('upload_image', this.uploadImages);
+    /**
+     * Add or edit a tour
+     * @param searchAddress search full address
+     */
+    saveTour(searchAddress) {
 
-        if (this.editCase) {
-            fd.append('id', data['id'])
-            this._tours.updateTour(fd).subscribe(dt => {
-                this.router.navigate(['/admin/AllTours']);
-            });
-        } else {
-            this._tours.insertTours(fd).subscribe((r: any) => {
-                this.router.navigate(['/admin/AllTours']);
-            });
+        if (this.saveTourForm.valid) {
+            this.formProcessing = true;
+            const data = this.saveTourForm.value;
+            const fd = new FormData();
+            fd.append('lat', data.lat);
+            fd.append('lng', data.lng);
+            fd.append('name', data.name);
+            fd.append('tours_type_id', data.tours_type_id);
+            fd.append('partner_id', data.partner_id);
+            fd.append('address', searchAddress.value);
+            fd.append('upload_image', this.uploadImages);
+
+            if (this.editCase) {
+                fd.append('id', data['id'])
+                this._tours.updateTour(fd).subscribe(dt => {
+                    this.formProcessing = false;
+                    this.router.navigate(['/admin/AllTours']);
+                    this.toastr.success('The tour info has been updated successfully', 'Updated!');
+                });
+            } else {
+                this._tours.insertTours(fd).subscribe((r: any) => {
+                    this.formProcessing = false;
+                    this.router.navigate(['/admin/AllTours']);
+                    this.toastr.success('The tour info has been added successfully', 'Added!');
+                });
+            }
         }
 
 
     }
+
+    get tourForm() {
+        return this.saveTourForm;
+    }
+
+    get nameCtrl() {
+        return this.tourForm.get('name');
+    }
+
+    get latCtrl() {
+        return this.tourForm.get('lat');
+    }
+
+    get lngCtrl() {
+        return this.tourForm.get('lng');
+    }
+
+    get addressCtrl() {
+        return this.tourForm.get('address');
+    }
+
 }
