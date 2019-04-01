0a2387ec24fbc0cfd4998645beb05531dddf5855
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-01 15:50:07 +0400

    Added http interceptor for error handling and common service for loaders handling

---
+++ src/app/shared/helpers/http.interceptor.ts
@@ -0,0 +1,38 @@
+import {tap} from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+    HttpResponse,
+    HttpErrorResponse,
+} from '@angular/common/http';
+
+import {Observable} from 'rxjs';
+
+import {ToastrService} from 'ngx-toastr';
+import {CommonService} from '../services/common.service';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+    constructor(public router: Router,
+                public toastr: ToastrService,
+                private common: CommonService
+    ) {
+    }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(tap((res: HttpResponse<any>) => {
+
+        }, (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+                this.common.formProcessing = false;
+                const message = err.error.message;
+                this.toastr.error(message.replace(/<(.|\n)*?>/g, ''));
+            }
+        }));
+    }
+}
