fe4747c098b06f92dd274fd5ea381e3db7ec6349
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-03 18:45:48 +0400

    Added email, latitude/longitude formats validation. Almost completed ferries form visual and validation

--- src/app/admin/all-food-drink/all-food-drink.component.ts
+++ src/app/admin/all-food-drink/all-food-drink.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FoodDrinkService} from '../services/food-drink.service';
 
 @Component({
-  selector: 'app-all-food-drink',
-  templateUrl: './all-food-drink.component.html',
-  styleUrls: ['./all-food-drink.component.scss']
+    selector: 'app-all-food-drink',
+    templateUrl: './all-food-drink.component.html',
+    styleUrls: ['./all-food-drink.component.scss']
 })
 export class AllFoodDrinkComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private _foodDrink: FoodDrinkService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/admin/ferry/save-ferry/save-ferry.component.ts
+++ src/app/admin/ferry/save-ferry/save-ferry.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FerryService} from '../../services/ferry.service';
 import {PartnerService} from '../../services/partner.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -6,6 +6,13 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {SPINNER_DIAMETER} from '../../../shared/constants/settings';
 import {ToastrService} from 'ngx-toastr';
 import {CommonService} from '../../../shared/services/common.service';
+import {patternValidator} from '../../../shared/helpers/pattern-validator';
+import {
+    EMAIL_PATTERN,
+    LATITUDE_PATTERN,
+    LONGITUDE_PATTERN,
+} from '../../../shared/constants/patterns';
+import {MapsAPILoader} from '@agm/core';
 
 @Component({
     selector: 'app-save-ferry',
@@ -20,6 +27,9 @@ export class SaveFerryComponent implements OnInit {
     partners;
     editCase = false;
 
+    @ViewChild('addressSearch')
+    public searchElementRef: ElementRef;
+
     constructor(
         private _fb: FormBuilder,
         private _ferry: FerryService,
@@ -27,7 +37,8 @@ export class SaveFerryComponent implements OnInit {
         private router: Router,
         private route: ActivatedRoute,
         private toastr: ToastrService,
-        public common: CommonService
+        public common: CommonService,
+        private mapsAPILoader: MapsAPILoader
     ) {
     }
 
@@ -35,57 +46,66 @@ export class SaveFerryComponent implements OnInit {
         this.editFerryForm = this._fb.group({
             'id': '',
             'name': ['', Validators.required],
-            'email': ['', Validators.required],
+            'email': ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
             'max_people': ['', Validators.required],
             'min_people': ['', Validators.required],
-            'lat': ['', Validators.required],
-            'lng': ['', Validators.required],
-            'phone': ['', Validators.required],
+            'lat': ['', [Validators.required, patternValidator(LATITUDE_PATTERN)]],
+            'lng': ['', [Validators.required, patternValidator(LONGITUDE_PATTERN)]],
+            'phone': ['', [Validators.required]],
             'address': ['', Validators.required],
             'type': 'ferry',
             'partner_id': ['', Validators.required]
         });
 
+        this.mapsAPILoader.load().then(() => {
+            if (this.searchElementRef) {
+                const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {types: ['geocode']});
+                const ferry_id = this.route.snapshot.paramMap.get('id');
+                if (ferry_id) {
+                    this.common.dataLoading = true;
+                    this.editCase = true;
+                    this._ferry.getOneFerry({id: ferry_id}).subscribe((dt: any) => {
+                        if (dt && dt.length > 0) {
+                            this.ferryData = dt[0];
+                            this.editFerryForm.patchValue(dt[0]);
+                            this._partner.getAllpartner().subscribe((d: any) => {
+                                this.partners = d['result'];
+                                this.common.dataLoading = false;
+                            });
+                        } else {
+                            this.common.dataLoading = false;
+                        }
+                    });
+                }
+            }
+        });
+
         this.getPartners();
 
-        const ferry_id = this.route.snapshot.paramMap.get('id');
-        if (ferry_id) {
-            this.common.dataLoading = true;
-            this.editCase = true;
-            this._ferry.getOneFerry({id: ferry_id}).subscribe((dt: any) => {
-                if (dt && dt.length > 0) {
-                    this.ferryData = dt[0];
-                    this.editFerryForm.patchValue(dt[0]);
-                    this._partner.getAllpartner().subscribe((d: any) => {
-                        this.partners = d['result'];
-                        this.common.dataLoading = false;
-                    });
-                } else {
-                    this.common.dataLoading = false;
-                }
-            });
-        }
+
 
 
     }
 
-    saveFerry() {
+    saveFerry(searchAddress) {
         const formValue = this.editFerryForm.value;
+        formValue.address = searchAddress.value;
+        console.log(formValue)
         // if (this.editFerryForm.valid) {
-            this.common.formProcessing = true;
-            if (this.editCase) {
-                this._ferry.update(formValue).subscribe(dt => {
-                    this.router.navigate(['admin/AllFerry']);
-                    this.toastr.success('The ferry info has been updated successfully', 'Updated!');
-                    this.common.formProcessing = false;
-                });
-            } else {
-                this._ferry.insertFerry(formValue).subscribe((r: any) => {
-                    this.router.navigate(['admin/AllFerry']);
-                    this.toastr.success('The ferry info has been added successfully', 'Added!');
-                    this.common.formProcessing = false;
-                });
-            }
+        this.common.formProcessing = true;
+        if (this.editCase) {
+            this._ferry.update(formValue).subscribe(() => {
+                this.router.navigate(['admin/AllFerry']);
+                this.toastr.success('The ferry info has been updated successfully', 'Updated!');
+                this.common.formProcessing = false;
+            });
+        } else {
+            this._ferry.insertFerry(formValue).subscribe(() => {
+                this.router.navigate(['admin/AllFerry']);
+                this.toastr.success('The ferry info has been added successfully', 'Added!');
+                this.common.formProcessing = false;
+            });
+        }
 
 
         // }
@@ -132,5 +152,6 @@ export class SaveFerryComponent implements OnInit {
     get minCtrl() {
         return this.ferryForm.get('min_people');
     }
+
 
 }
