989c3fc47bcb26fa10e50cbb77e859434cef8972
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-04 18:59:12 +0400

    Applied menu style changes

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -2,185 +2,214 @@ import {Router} from "@angular/router";
 
 import {CollectionViewer, SelectionChange} from '@angular/cdk/collections';
 import {FlatTreeControl} from '@angular/cdk/tree';
-import {Component, OnInit, Injectable} from '@angular/core';
+import {Component, OnInit, Injectable, ViewChild} from '@angular/core';
 import {BehaviorSubject, merge, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {MENU_ITEM_ICONS} from '../../shared/constants/settings';
 
 export class DynamicFlatNode {
-  constructor(public item: string, public level = 1, public expandable = false,
-              public isLoading = false) {
-  }
+    constructor(public item: string, public level = 1, public expandable = false,
+                public isLoading = false) {
+    }
 }
 
 export class DynamicDatabase {
-  dataMap = new Map<string, string[]>([
-    ['Ferry', ['Add Ferry', 'All Ferry']],
-    ['Tours', ['Add Tours', 'All Tours', 'Add Tours Type', 'All Tours Type']],
-    ['Food-Drink', ['Add Food-Drink', 'All Food-Drink']],
-    ['Partner', ['Add Partner', 'All Partner']],
-    ['Gps Location', ['Add location']],
-  ]);
+    dataMap = new Map<string, string[]>([
+        ['Ferries', ['Add Ferry', 'All Ferries']],
+        ['Tours', ['Add Tour', 'All Tours', 'Add Tours Type', 'All Tour Types']],
+        ['Food-Drink', ['Add Food-Drink', 'All Food-Drink']],
+        ['Partners', ['Add Partner', 'All Partners']],
+        ['Gps Location', ['Add Location']],
+    ]);
 
-  rootLevelNodes: string[] = ['Ferry', 'Tours', 'Food-Drink', 'Partner', 'Gps Location'];
+    rootLevelNodes: string[] = ['Ferries', 'Tours', 'Food-Drink', 'Partners', 'Gps Location'];
 
-  /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
-  }
+    /** Initial data from database */
+    initialData(): DynamicFlatNode[] {
+        return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
+    }
 
-  getChildren(node: string): string[] | undefined {
-    return this.dataMap.get(node);
-  }
+    getChildren(node: string): string[] | undefined {
+        return this.dataMap.get(node);
+    }
 
-  isExpandable(node: string): boolean {
-    return this.dataMap.has(node);
-  }
+    isExpandable(node: string): boolean {
+        return this.dataMap.has(node);
+    }
 }
 
 @Injectable()
 export class DynamicDataSource {
 
-  dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
+    dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
 
-  get data(): DynamicFlatNode[] {
-    return this.dataChange.value;
-  }
-
-  set data(value: DynamicFlatNode[]) {
-    this.treeControl.dataNodes = value;
-    this.dataChange.next(value);
-  }
-
-  constructor(private treeControl: FlatTreeControl<DynamicFlatNode>,
-              private database: DynamicDatabase) {
-  }
-
-  connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
-    this.treeControl.expansionModel.onChange.subscribe(change => {
-      if ((change as SelectionChange<DynamicFlatNode>).added ||
-        (change as SelectionChange<DynamicFlatNode>).removed) {
-        this.handleTreeControl(change as SelectionChange<DynamicFlatNode>);
-      }
-    });
-
-    return merge(collectionViewer.viewChange, this.dataChange).pipe(map(() => this.data));
-  }
-
-  /** Handle expand/collapse behaviors */
-  handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
-    if (change.added) {
-      change.added.forEach(node => this.toggleNode(node, true));
-    }
-    if (change.removed) {
-      change.removed.slice().reverse().forEach(node => this.toggleNode(node, false));
-    }
-  }
-
-  /**
-   * Toggle the node, remove from display list
-   */
-  toggleNode(node: DynamicFlatNode, expand: boolean) {
-    const children = this.database.getChildren(node.item);
-    const index = this.data.indexOf(node);
-    if (!children || index < 0) { // If no children, or cannot find the node, no op
-      return;
+    get data(): DynamicFlatNode[] {
+        return this.dataChange.value;
     }
 
-    // node.isLoading = true;
+    set data(value: DynamicFlatNode[]) {
+        this.treeControl.dataNodes = value;
+        this.dataChange.next(value);
+    }
 
-    // setTimeout(() => {
-      if (expand) {
-        const nodes = children.map(name =>
-          new DynamicFlatNode(name, node.level + 1, this.database.isExpandable(name)));
-        this.data.splice(index + 1, 0, ...nodes);
-      } else {
-        let count = 0;
-        for (let i = index + 1; i < this.data.length
-        && this.data[i].level > node.level; i++, count++) {
+    constructor(private treeControl: FlatTreeControl<DynamicFlatNode>,
+                private database: DynamicDatabase) {
+    }
+
+    connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
+        this.treeControl.expansionModel.onChange.subscribe(change => {
+            if ((change as SelectionChange<DynamicFlatNode>).added ||
+                (change as SelectionChange<DynamicFlatNode>).removed) {
+                this.handleTreeControl(change as SelectionChange<DynamicFlatNode>);
+            }
+        });
+
+        return merge(collectionViewer.viewChange, this.dataChange).pipe(map(() => this.data));
+    }
+
+    /** Handle expand/collapse behaviors */
+    handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
+        if (change.added) {
+            change.added.forEach(node => this.toggleNode(node, true));
         }
-        this.data.splice(index + 1, count);
-      }
+        if (change.removed) {
+            change.removed.slice().reverse().forEach(node => this.toggleNode(node, false));
+        }
+    }
 
-      // notify the change
-      this.dataChange.next(this.data);
-      // node.isLoading = false;
-    // }, 1000);
-  }
+    /**
+     * Toggle the node, remove from display list
+     */
+    toggleNode(node: DynamicFlatNode, expand: boolean) {
+        const children = this.database.getChildren(node.item);
+        const index = this.data.indexOf(node);
+        if (!children || index < 0) { // If no children, or cannot find the node, no op
+            return;
+        }
+
+        // node.isLoading = true;
+
+        // setTimeout(() => {
+        if (expand) {
+            const nodes = children.map(name =>
+                new DynamicFlatNode(name, node.level + 1, this.database.isExpandable(name)));
+            this.data.splice(index + 1, 0, ...nodes);
+        } else {
+            let count = 0;
+            for (let i = index + 1; i < this.data.length
+            && this.data[i].level > node.level; i++, count++) {
+            }
+            this.data.splice(index + 1, count);
+        }
+
+        // notify the change
+        this.dataChange.next(this.data);
+        // node.isLoading = false;
+        // }, 1000);
+    }
 }
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  providers: [DynamicDatabase]
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    providers: [DynamicDatabase]
 })
 
 export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router, database: DynamicDatabase) {
-    this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
-    this.dataSource = new DynamicDataSource(this.treeControl, database);
+    @ViewChild('drawer') drawer;
 
-    this.dataSource.data = database.initialData();
-  }
+    constructor(private router: Router, database: DynamicDatabase) {
+        this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
+        this.dataSource = new DynamicDataSource(this.treeControl, database);
 
-  showFiller = false;
-
-  treeControl: FlatTreeControl<DynamicFlatNode>;
-
-  dataSource: DynamicDataSource;
-
-  getLevel = (node: DynamicFlatNode) => node.level;
-
-  isExpandable = (node: DynamicFlatNode) => node.expandable;
-
-  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
-
-  ngOnInit() {
-
-    if (!this.checkAdmin()) {
-      this.router.navigate(['admin-panel']);
-    }
-  }
-
-  changeWidth(drawer) {
-
-    if (!drawer) {
-      this.showFiller = true;
-    } else {
-      this.showFiller = false;
-    }
-  }
-
-  checkAdmin() {
-    let jsAdminInf = localStorage.getItem('adminInf');
-    if (typeof jsAdminInf == 'undefined') {
-      return false;
+        this.dataSource.data = database.initialData();
     }
 
-    let adminInf = JSON.parse(jsAdminInf);
+    showFiller = false;
 
-    if (adminInf == null) {
-      return false;
+    treeControl: FlatTreeControl<DynamicFlatNode>;
+
+    dataSource: DynamicDataSource;
+
+    getLevel = (node: DynamicFlatNode) => node.level;
+
+    isExpandable = (node: DynamicFlatNode) => node.expandable;
+
+    hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+
+    ngOnInit() {
+
+        if (!this.checkAdmin()) {
+            this.router.navigate(['admin-panel']);
+        }
+        this.drawer.toggle();
+        this.showFiller = true;
     }
 
-    if (adminInf['admin_session_inf'] == '') {
-      return false;
+    ngAfterViewInit() {
+        for (let i = 0; i < this.treeControl.dataNodes.length; i++) {
+            if (this.treeControl.dataNodes[i].item == 'Tours') {
+                this.treeControl.expand(this.treeControl.dataNodes[i])
+            }
+            // if (this.treeControl.dataNodes[i].item == 'Groceries') {
+            //     this.treeControl.expand(this.treeControl.dataNodes[i])
+            // }
+        }
     }
 
-    return true;
-  }
+    changeWidth(drawer) {
 
-  routing(router_name) {
+        if (!drawer) {
+            this.showFiller = true;
+        } else {
+            this.showFiller = false;
+        }
+    }
 
-    let router = router_name.split(' ');
+    checkAdmin() {
+        let jsAdminInf = localStorage.getItem('adminInf');
+        if (typeof jsAdminInf == 'undefined') {
+            return false;
+        }
 
-    this.router.navigate(['/admin/' + router.join('')]);
-  }
+        let adminInf = JSON.parse(jsAdminInf);
 
-  logout() {
-    localStorage.removeItem('adminInf');
-    this.router.navigate(['admin-panel']);
-  }
+        if (adminInf == null) {
+            return false;
+        }
+
+        if (adminInf['admin_session_inf'] == '') {
+            return false;
+        }
+
+        return true;
+    }
+
+    routing(name) {
+        name = name.replace('All ', '');
+        const route = name.replace(/ /g, '_').toLowerCase();
+
+        this.router.navigate(['/admin/' + route]);
+    }
+
+    logout() {
+        localStorage.removeItem('adminInf');
+        this.router.navigate(['admin-panel']);
+    }
+
+    getIcon(item) {
+        let icon = '';
+        item = item.toLowerCase();
+        MENU_ITEM_ICONS.map(mi => {
+            if (item.includes(mi['item'])) {
+                icon = mi['icon'];
+            } else if (item.includes('add')) {
+                icon = 'add';
+            }
+        });
+        return icon;
+    }
 
 }
