aac6889a0acd2ef07b31f88be19e9c5d13c7e638
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-05 18:44:35 +0400

    Added latest changes, active-links works everywhere, disabled food-drink temporarily

--- src/app/admin/add-food-drink/add-food-drink.component.ts
+++ src/app/admin/add-food-drink/add-food-drink.component.ts
@@ -27,96 +27,96 @@ export class AddFoodDrinkComponent implements OnInit {
   constructor(private mapsAPILoader: MapsAPILoader, private http: HttpClient, private router: Router, private  foodDrinkService: FoodDrinkService, private partner: PartnerService) {
   }
 
-  partnersTypeName: any = [];
-
-  nameFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  latFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  lngFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  partnersTypeControl = new FormControl('', [
-    Validators.required
-  ]);
-
-  descFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
-
-  foodDrink = {name: '', partner_id: '', desc: '', address: '', img: '', lat: '', lng: ''};
-  upload_images = null;
-
+  // partnersTypeName: any = [];
+  //
+  // nameFormControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  //
+  // latFormControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  //
+  // lngFormControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  //
+  // partnersTypeControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  //
+  // descFormControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  //
+  //
+  // foodDrink = {name: '', partner_id: '', desc: '', address: '', img: '', lat: '', lng: ''};
+  // upload_images = null;
+  //
   ngOnInit() {
-    if (!this.checkAdmin()) {
-      this.router.navigate(['admin-panel']);
-    }
-    this.getPartners();
-    this.mapsAPILoader.load().then(() => {
-      const autocomplete = new google.maps.places.Autocomplete(this.searchelementRef.nativeElement, {types: ['geocode']});
-    });
+    // if (!this.checkAdmin()) {
+    //   this.router.navigate(['admin-panel']);
+    // }
+    // this.getPartners();
+    // this.mapsAPILoader.load().then(() => {
+    //   const autocomplete = new google.maps.places.Autocomplete(this.searchelementRef.nativeElement, {types: ['geocode']});
+    // });
   }
-
-  getPartners() {
-    this.partner.getAllpartner().subscribe((r: any) => {
-
-      if (r.status == 0) {
-        alert(r['message']);
-        return false;
-      }
-
-      r['result'].map(k => this.partnersTypeName.push(k));
-    });
-  }
-
-  saveFoodDrink(data) {
-
-    const fd = new FormData();
-
-    fd.append('lat', data.lat);
-    fd.append('lng', data.lng);
-    fd.append('name', data.name);
-    fd.append('partner_id', data.partner_id);
-    fd.append('desc', data.desc);
-    fd.append('address', data.address);
-    fd.append('upload_image', this.upload_images);
-    this.foodDrinkService.insertFoodDrink(fd).subscribe((r: any) => {
-
-      if (r.status == 0) {
-        alert(r['message']);
-        return false;
-      }
-
-      this.router.navigate(['/admin/AllFood-Drink']);
-    });
-  }
-
-  handleFileInput(files: FileList) {
-    this.upload_images = files.item(0);
-  }
-
-  checkAdmin() {
-    let jsAdminInf = localStorage.getItem('adminInf');
-    if (typeof jsAdminInf == 'undefined') {
-      return false;
-    }
-
-    let adminInf = JSON.parse(jsAdminInf);
-
-    if (adminInf == null) {
-      return false;
-    }
-
-    if (adminInf['admin_session_inf'] == '') {
-      return false;
-    }
-
-    return true;
-  }
+  //
+  // getPartners() {
+  //   this.partner.getAllpartner().subscribe((r: any) => {
+  //
+  //     if (r.status == 0) {
+  //       alert(r['message']);
+  //       return false;
+  //     }
+  //
+  //     r['result'].map(k => this.partnersTypeName.push(k));
+  //   });
+  // }
+  //
+  // saveFoodDrink(data) {
+  //
+  //   const fd = new FormData();
+  //
+  //   fd.append('lat', data.lat);
+  //   fd.append('lng', data.lng);
+  //   fd.append('name', data.name);
+  //   fd.append('partner_id', data.partner_id);
+  //   fd.append('desc', data.desc);
+  //   fd.append('address', data.address);
+  //   fd.append('upload_image', this.upload_images);
+  //   this.foodDrinkService.insertFoodDrink(fd).subscribe((r: any) => {
+  //
+  //     if (r.status == 0) {
+  //       alert(r['message']);
+  //       return false;
+  //     }
+  //
+  //     this.router.navigate(['/admin/AllFood-Drink']);
+  //   });
+  // }
+  //
+  // handleFileInput(files: FileList) {
+  //   this.upload_images = files.item(0);
+  // }
+  //
+  // checkAdmin() {
+  //   let jsAdminInf = localStorage.getItem('adminInf');
+  //   if (typeof jsAdminInf == 'undefined') {
+  //     return false;
+  //   }
+  //
+  //   let adminInf = JSON.parse(jsAdminInf);
+  //
+  //   if (adminInf == null) {
+  //     return false;
+  //   }
+  //
+  //   if (adminInf['admin_session_inf'] == '') {
+  //     return false;
+  //   }
+  //
+  //   return true;
+  // }
 }
