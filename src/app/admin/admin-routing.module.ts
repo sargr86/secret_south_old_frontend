fa20204920435f60fa0326b5d6874c409eec7f1c
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-16 18:07:28 +0400

    Partially removed additional components created earlier

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {AddFerryComponent} from './add-ferry/add-ferry.component';
-import {AllFerryComponent} from './all-ferry/all-ferry.component';
 import {AddToursComponent} from './add-tours/add-tours.component';
 import {AllToursComponent} from './all-tours/all-tours.component';
 import {AddToursTypeComponent} from './add-tours-type/add-tours-type.component';
 import {AllToursTypeComponent} from './all-tours-type/all-tours-type.component';
 import {AddFoodDrinkComponent} from './add-food-drink/add-food-drink.component';
 import {AllFoodDrinkComponent} from './all-food-drink/all-food-drink.component';
-import {AddPartnerComponent} from './add-partner/add-partner.component';
-import {AllPartnerComponent} from './all-partner/all-partner.component';
 import {GpsLocationComponent} from './gps-location/gps-location.component';
 import {EditFerryComponent} from './edit-ferry/edit-ferry.component';
 import {ShowFerriesComponent} from './ferry/show-ferries/show-ferries.component';
