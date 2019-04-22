ecb5b39d90c83440e2f6a1e9e0ae201d8292d390
Grigor Sargsyan <sargr86@gmail.com>
2019-04-22 17:00:03 +0400

    Added food-drink crud

---
+++ src/app/shared/resolvers/one-food-drink-resolver.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
+import {FoodDrinkService} from '../../admin/services/food-drink.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class OneFoodDrinkResolverService implements Resolve<any> {
+
+    constructor(
+        private _foodDrink: FoodDrinkService
+    ) {
+
+    }
+
+    resolve(route: ActivatedRouteSnapshot) {
+        return this._foodDrink.getOneFoodDrink({id: route.params.id});
+    }
+}
