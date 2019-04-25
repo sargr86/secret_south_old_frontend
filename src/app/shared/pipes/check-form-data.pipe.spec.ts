0b4213f22e38b9858b7d0308bbebfcbf14d4e141
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-25 18:01:12 +0400

    Added check form data pipe

---
+++ src/app/shared/pipes/check-form-data.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CheckFormDataPipe } from './check-form-data.pipe';
+
+describe('CheckFormDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CheckFormDataPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
