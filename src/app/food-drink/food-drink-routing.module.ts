d434ebda4ac039bc57f619a588e950c084ef398d
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-02 14:14:00 +0400

    Route fixes

--- src/app/food-drink/food-drink-routing.module.ts
+++ src/app/food-drink/food-drink-routing.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SaveFoodDrinkComponent} from './save-food-drink/save-food-drink.component';
-import {AuthGuard} from '../shared/guards/auth.guard';
-import {RoleGuard} from '../shared/guards/role.guard';
 import {ShowFoodDrinkComponent} from './show-food-drink/show-food-drink.component';
-import {ShowPartnersComponent} from '../partners/show-partners/show-partners.component';
 import {OneFoodDrinkResolverService} from '../shared/resolvers/one-food-drink-resolver.service';
 
 const routes: Routes = [
@@ -12,17 +9,13 @@ const routes: Routes = [
     {
         path: 'add', component: SaveFoodDrinkComponent, data: {
             title: 'Add a new food/drink place',
-            expectedRole: 'admin'
         },
-        canActivate: [AuthGuard, RoleGuard],
     },
 
     {
         path: ':id', component: SaveFoodDrinkComponent, data: {
             title: 'Edit a food/drink info',
-            expectedRole: 'admin'
         },
-        canActivate: [AuthGuard, RoleGuard],
         resolve: {
             oneFoodDrink: OneFoodDrinkResolverService
         }
--- src/app/tours/tours-routing.module.ts
+++ src/app/tours/tours-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ShowToursComponent} from './show-tours/show-tours.component';
-import {AuthGuard} from '../shared/guards/auth.guard';
-import {RoleGuard} from '../shared/guards/role.guard';
 import {SaveTourComponent} from './save-tour/save-tour.component';
 import {OneTourResolverService} from '../shared/resolvers/one-tour-resolver.service';
 import {ShowTourTypesComponent} from './show-tour-types/show-tour-types.component';
@@ -30,23 +28,17 @@ const routes: Routes = [
   {
     path: 'show-types', component: ShowTourTypesComponent, data: {
       title: 'All tour types',
-      expectedRole: 'admin'
     },
-    canActivate: [AuthGuard, RoleGuard],
   },
   {
-    path: 'tour_type/:id', component: SaveTourTypeComponent, data: {
+    path: 'tour-type/:id', component: SaveTourTypeComponent, data: {
       title: 'Edit a tour type info',
-      expectedRole: 'admin'
     },
-    canActivate: [AuthGuard, RoleGuard],
   },
   {
     path: 'add-types', component: SaveTourTypeComponent, data: {
       title: 'Add a new tour type',
-      expectedRole: 'admin'
     },
-    canActivate: [AuthGuard, RoleGuard],
   }
 
 ];
