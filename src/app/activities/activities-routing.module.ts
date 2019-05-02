f7be1919dfda605f6ce9f78862f9ce7c9d79db77
sargr86 <sargr86@gmail.com>
2019-05-02 12:05:35 +0400

    Separated admin & partner routes to specific lazy-loading modules routes

--- src/app/activities/activities-routing.module.ts
+++ src/app/activities/activities-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ShowActivityTypesComponent} from './show-activity-types/show-activity-types.component';
+import {SaveActivityTypeComponent} from './save-activity-type/save-activity-type.component';
+import {OneAccommodationResolverService} from '../shared/resolvers/one-accommodation-resolver.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'show-types', component: ShowActivityTypesComponent},
+  {
+    path: 'add-types', component: SaveActivityTypeComponent,
+    data: {
+      title: 'Add activity type',
+    },
+  },
+  {
+    path: ':id', component: SaveActivityTypeComponent, data: {
+      title: 'Edit an activity type',
+    },
+    resolve: {
+      oneFoodDrink: OneAccommodationResolverService
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/partners/partners-routing.module.ts
+++ src/app/partners/partners-routing.module.ts
@@ -1,91 +1,79 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {ShowFerriesComponent} from '../ferries/show-ferries/show-ferries.component';
-import {MainDashboardComponent} from '../shared/components/main-dashboard/main-dashboard.component';
-import {SaveFerryComponent} from '../ferries/save-ferry/save-ferry.component';
 import {ShowProfileComponent} from './show-profile/show-profile.component';
 import {EditProfileComponent} from './edit-profile/edit-profile.component';
-import {OneFerryResolverService} from '../shared/resolvers/one-ferry-resolver.service';
-import {ShowToursComponent} from '../tours/show-tours/show-tours.component';
-import {SaveTourComponent} from '../tours/save-tour/save-tour.component';
 import {AuthGuard} from '../shared/guards/auth.guard';
-import {OneTourResolverService} from '../shared/resolvers/one-tour-resolver.service';
-import {ShowTourTypesComponent} from '../tours/show-tour-types/show-tour-types.component';
-import {ShowFoodDrinkComponent} from '../food-drink/show-food-drink/show-food-drink.component';
+import {RoleGuard} from '../shared/guards/role.guard';
+import {ShowPartnersComponent} from './show-partners/show-partners.component';
+import {SavePartnerComponent} from './save-partner/save-partner.component';
 
 
 const routes: Routes = [
     {
-        path: 'dashboard',
-        component: ShowProfileComponent,
-        canActivate: [AuthGuard]
+        path: '',
+        component: ShowPartnersComponent
     },
     {
-        path: 'dashboard/show',
+        path: 'add',
+        component: SavePartnerComponent
+    },
+
+    {
+        path: 'dashboard',
         component: ShowProfileComponent,
-        canActivate: [AuthGuard]
     },
     {
         path: 'dashboard/edit',
         component: EditProfileComponent,
-        canActivate: [AuthGuard]
     },
+
     {
-        path: 'ferries/show',
-        component: ShowFerriesComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'ferries/add',
-        component: SaveFerryComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'ferry/:id',
-        component: SaveFerryComponent,
-        resolve: {
-            oneFerry: OneFerryResolverService
+        path: 'ferries',
+        loadChildren: '../ferries/ferries.module#FerriesModule',
+        data: {
+            expectedRole: 'partner'
         },
-        canActivate: [AuthGuard]
+        canActivate: [RoleGuard]
     },
     {
-        path: 'tours/show',
-        component: ShowToursComponent,
-        canActivate: [AuthGuard]
+        path: 'tours',
+        loadChildren: '../tours/tours.module#ToursModule',
+        data: {
+            expectedRole: 'partner'
+        },
+        canActivate: [AuthGuard, RoleGuard]
     },
     {
-        path: 'tours/show-types',
-        component: ShowTourTypesComponent,
-        canActivate: [AuthGuard]
+        path: 'food-drink',
+        loadChildren: '../food-drink/food-drink.module#FoodDrinkModule',
+        data: {
+            expectedRole: 'partner'
+        },
+        canActivate: [AuthGuard, RoleGuard]
     },
-    // {
-    //     path: 'tours/add',
-    //     component: SaveTourComponent,
-    //     canActivate: [ AuthGuard]
-    // },
-    // {
-    //     path: 'tour/:id',
-    //     component: SaveTourComponent,
-    //     resolve: {
-    //         oneTour: OneTourResolverService
-    //     },
-    //     canActivate: [ AuthGuard]
-    // },
+
     {
-        path: 'accommodations/show',
-        component: ShowToursComponent,
-        canActivate: [AuthGuard]
+        path: 'accommodations',
+        loadChildren: '../accommodation/accommodation.module#AccommodationModule',
+        data: {
+            expectedRole: 'partner'
+        },
+        canActivate: [AuthGuard, RoleGuard]
     },
-    // {
-    //     path: 'accommodations/add',
-    //     component: SaveTourComponent,
-    //     canActivate: [ AuthGuard]
-    // },
+
     {
-        path: 'food-drink/show',
-        component: ShowFoodDrinkComponent,
-        canActivate: [AuthGuard]
-    }
+        path: 'activities',
+        loadChildren: '../activities/activities.module#ActivitiesModule',
+        data: {
+            expectedRole: 'partner'
+        },
+        canActivate: [AuthGuard, RoleGuard]
+    },
+    {
+        path: ':id',
+        component: SavePartnerComponent
+    },
+
 ];
 
 @NgModule({
