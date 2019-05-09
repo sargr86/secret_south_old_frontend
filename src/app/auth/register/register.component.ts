6574d6ec01be3533d034001ee27fc814d470885c
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-09 13:33:31 +0400

    Added 'employee' role to the seeder, implemented partner registration

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -5,6 +5,7 @@ import {CommonService} from '../../shared/services/common.service';
 import {Router} from '@angular/router';
 import {SPINNER_DIAMETER} from '../../shared/constants/settings';
 import {DROPZONE_CONFIG} from 'ngx-dropzone-wrapper';
+import * as jwtDecode from 'jwt-decode';
 
 @Component({
     selector: 'app-register',
@@ -18,6 +19,7 @@ export class RegisterComponent implements OnInit {
     redirectUrl = '/';
     spinnerDiameter = SPINNER_DIAMETER;
     dropzoneConfig = DROPZONE_CONFIG;
+    passHidden = false;
 
     constructor(
         private _fb: FormBuilder,
@@ -38,7 +40,39 @@ export class RegisterComponent implements OnInit {
     }
 
     register() {
+        const formData = this.getFormData();
+        this.auth.register(formData).subscribe((dt: any) => {
+            // Saving token to browser local storage
+            localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
 
+            // Gets current user data
+            this.auth.userData = jwtDecode(localStorage.getItem('token'));
+
+
+            // Navigate to the home page
+            this.router.navigate([this.auth.checkRoles('admin') ? 'admin/dashboard' : 'partners/dashboard']);
+        });
+    }
+
+
+    getFormData() {
+        const formData = new FormData();
+        for (const field in this.registerForm.value) {
+            formData.append(field, this.registerForm.value[field]);
+        }
+        const files = this.dropzoneFile;
+        if (files && files.length > 0) {
+            files.map(file => {
+                if (file['name']) {
+                    const nameArr = file['name'].split('.');
+                    const fileName = `${nameArr[0]}.${nameArr[1]}`;
+                    formData.append('profile_img', fileName);
+                    formData.append('profile_img_file', file, fileName);
+                }
+
+            });
+        }
+        return formData;
     }
 
     removeImage() {
@@ -50,6 +84,10 @@ export class RegisterComponent implements OnInit {
      */
     onAddedFile(e) {
         this.dropzoneFile = e;
+    }
+
+    togglePass() {
+        this.passHidden = !this.passHidden;
     }
 
     /**
