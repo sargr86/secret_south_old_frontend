b24933a73c5cfa12c563cb82cd87691dafb7fdd6
sargr86 <sargr86@gmail.com>
2019-05-23 14:35:04 +0400

    Improvements for invitations/register process

--- src/app/shared/components/invitation-form/invitation-form.component.ts
+++ src/app/shared/components/invitation-form/invitation-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CommonService} from '../../services/common.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {SPINNER_DIAMETER, USER_TYPES} from '../../constants/settings';
 import {AuthService} from '../../services/auth.service';
 import {PartnerService} from '../../services/partner.service';
@@ -17,11 +17,12 @@ export class InvitationFormComponent implements OnInit {
     invitationForm: FormGroup;
     spinnerDiameter = SPINNER_DIAMETER;
     redirectUrl;
-    userTypes = USER_TYPES;
+    userType;
     partnerTypes;
     fields = {
         'first_name': ['', Validators.required],
         'last_name': ['', Validators.required],
+        'gender': ['', Validators.required],
         'email': ['', Validators.required],
         'field_type': ['', Validators.required],
     };
@@ -33,18 +34,24 @@ export class InvitationFormComponent implements OnInit {
         public auth: AuthService,
         private _partners: PartnerService,
         private _employees: EmployeesService,
-        private toastr: ToastrService
+        private toastr: ToastrService,
+        private route: ActivatedRoute
     ) {
     }
 
     ngOnInit() {
         this.invitationForm = this._fb.group(this.fields);
-        if (this.auth.checkRoles('admin')) {
-            this.fields['user_type'] = ['', Validators.required];
-            this.invitationForm = this._fb.group(this.fields);
-        }
+
         this._partners.getTypes().subscribe(d => {
             this.partnerTypes = d;
+        });
+
+        this.route.data.subscribe(dt => {
+            this.userType = dt.user_type;
+            // if (this.auth.checkRoles('admin')) {
+            this.fields['user_type'] = [this.userType, Validators.required];
+            this.invitationForm = this._fb.group(this.fields);
+            // }
         });
     }
 
@@ -60,15 +67,27 @@ export class InvitationFormComponent implements OnInit {
         return this.invitationForm.get('email');
     }
 
+    get fieldType(): AbstractControl {
+        return this.invitationForm.get('field_type');
+    }
+
+    /**
+     * Inviting a partner/employee
+     */
     invite() {
-        const userType = this.invitationForm.value['user_type'];
-        this.common.formProcessing = true;
-        this[`_${userType}`].invite(this.invitationForm.value).subscribe(dt => {
-            this.toastr.success('The invitation has been sent successfully');
-            this.redirectUrl = (this.auth.checkRoles('admin') ? 'admin/' : 'partners/') + userType;
-            this.router.navigate([this.redirectUrl]);
-            this.common.formProcessing = false;
-        });
+        const userType = USER_TYPES.filter(t => t['role'] === this.invitationForm.value['user_type']);
+
+        if (userType.length > 0) {
+            this.common.formProcessing = true;
+            const type = userType[0]['label'];
+
+            this[`_${type}`].invite(this.invitationForm.value).subscribe(dt => {
+                this.toastr.success('The invitation has been sent successfully');
+                this.redirectUrl = (this.auth.checkRoles('admin') ? 'admin/' : 'partners/') + type;
+                this.router.navigate([this.redirectUrl]);
+                this.common.formProcessing = false;
+            });
+        }
 
     }
 
