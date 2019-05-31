b285058993537de84412fa77c3d4602dd35a745c
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-31 18:55:02 +0400

    Added company fields to almost all sections, in front-end added to activities, also for partners

--- src/app/activities/activities-routing.module.ts
+++ src/app/activities/activities-routing.module.ts
@@ -14,31 +14,39 @@ const routes: Routes = [
     },
     {
         path: 'add',
-        component: SaveActivityComponent
+        component: SaveActivityComponent,
+        data: {
+            title: 'Add an activity provider',
+        },
     },
 
 
-    {path: 'show-types', component: ShowActivityTypesComponent},
+    {
+        path: 'show-types', component: ShowActivityTypesComponent, data: {
+            title: 'Show activity types'
+        }
+    },
     {
         path: 'add-types', component: SaveActivityTypeComponent,
         data: {
             title: 'Add activity type',
         },
     },
-    {
-        path: 'activities/:id', component: SaveActivityComponent, data: {
-            title: 'Edit an  activity provider info',
-        },
-        resolve: {
-            activity: ActivityResolverService
-        }
-    },
+
     {
         path: 'activity-type/:id', component: SaveActivityTypeComponent, data: {
             title: 'Edit an activity type',
         },
         resolve: {
             // oneFoodDrink: OneAccommodationResolverService
+        }
+    },
+    {
+        path: ':id', component: SaveActivityComponent, data: {
+            title: 'Edit an  activity provider info',
+        },
+        resolve: {
+            activity: ActivityResolverService
         }
     },
 ];
