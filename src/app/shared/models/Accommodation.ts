7c74a4a1ac0baf3e3e4f1b10e858a05a5a1ed348
Grigor Sargsyan <sargr86@gmail.com>
2019-06-06 13:51:05 +0400

    Optimized accommodations form, created Accommodation & Company models

---
+++ src/app/shared/models/Accommodation.ts
@@ -0,0 +1,9 @@
+export interface Accommodation {
+    id?: number;
+    company_id?: number;
+    name?: string;
+    description?: string;
+    address?: string;
+    lat?: number;
+    lng?: number;
+}
