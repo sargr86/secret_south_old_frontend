dc4c65059a65ca9af911231dff1180c6d3919d71
Sofia Bruno <sofiabruno3003@gmail.com>
2019-06-14 12:04:51 +0400

    Fixed sidenav appearance for auth routes, added get-customers route

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {ShowCustomersComponent} from './show-customers/show-customers.component';
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  }
+    {
+        path: '',
+        component: ShowCustomersComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {
+}
