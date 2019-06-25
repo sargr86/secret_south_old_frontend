ff87b4938114002541f1342f0856a98b369088f4
Sofia Bruno <sofiabruno19@yahoo.com>
2019-06-25 12:11:53 +0400

    Added missing parts

---
+++ src/app/shared/pipes/redirect-url-generator.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {AuthService} from '../services/auth.service';
+
+@Pipe({
+    name: 'redirectUrlGenerator'
+})
+export class RedirectUrlGeneratorPipe implements PipeTransform {
+    constructor(private auth: AuthService) {
+    }
+
+    transform(item: any, args?: any): any {
+        return `${this.auth.checkRoles('admin') ? 'admin' : 'partners'}/${item}`;
+    }
+
+}
