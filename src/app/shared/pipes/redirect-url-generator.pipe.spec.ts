d5dbd3735a7ca06aa40907e1b19da9fd65f2cfef
Sofia Bruno <sofiabruno3003@gmail.com>
2019-06-25 12:11:53 +0400

    Added missing parts

---
+++ src/app/shared/pipes/redirect-url-generator.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RedirectUrlGeneratorPipe } from './redirect-url-generator.pipe';
+
+describe('RedirectUrlGeneratorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RedirectUrlGeneratorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
