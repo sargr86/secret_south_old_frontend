e7839031d096f476b750e0b4c49e3074d6786a7d
Grigor Sargsyan <sargr86@gmail.com>
2019-07-01 14:17:54 +0400

    Fixed file size error appearance

--- src/app/shared/helpers/http.interceptor.ts
+++ src/app/shared/helpers/http.interceptor.ts
@@ -34,21 +34,20 @@ export class RequestInterceptor implements HttpInterceptor {
                 const message = err.error.message;
 
                 // if (!this.common.showOverlay) {
-                    // Sequelize db connection error
-                    if (err.error.hasOwnProperty('db_error')) {
-                        this.toastr.error(err.error.db_error, 'Unable to connect to database');
+                // Sequelize db connection error
+                if (err.error.hasOwnProperty('db_error')) {
+                    this.toastr.error(err.error.db_error, 'Unable to connect to database');
 
-                        // Server connection error
-                    } else if (err.status === 200 || err.status === 0 || err.error.hasOwnProperty('conn_error')) {
-                        this.toastr.error('Please check server connection.', 'Unable to connect to server');
-                    } else {
-
-                        if (err.error.hasOwnProperty('msg')) {
-                            this.toastr.error('', err.error.msg);
-                        } else if (message) {
-                            this.toastr.error(message.replace(/<(.|\n)*?>/g, ''));
-                        }
+                    // Server connection error
+                } else if (err.status === 200 || err.status === 0 || err.error.hasOwnProperty('conn_error')) {
+                    this.toastr.error('Please check server connection.', 'Unable to connect to server');
+                } else {
+                    if (err.error.hasOwnProperty('msg')) {
+                        this.toastr.error('', err.error.msg);
+                    } else if (message) {
+                        this.toastr.error(message.replace(/<(.|\n)*?>/g, ''));
                     }
+                }
                 // }
 
 
