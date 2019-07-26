fb3844a91eeb0a2e7e1ee32ed5402cb5a024c3e7
Sofia Bruno <sofiabruno3003@gmail.com>
2019-07-26 15:02:39 +0400

    Added food-drink section sliders

--- src/app/food-drink/food-drink-home/food-drink-home.component.ts
+++ src/app/food-drink/food-drink-home/food-drink-home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SubjectService} from '../../shared/services/subject.service';
+import {FOOD_DRINK_FOLDER} from '../../shared/constants/settings';
+import {MainService} from '../../home/services/main.service';
 
 @Component({
-  selector: 'app-food-drink-home',
-  templateUrl: './food-drink-home.component.html',
-  styleUrls: ['./food-drink-home.component.scss']
+    selector: 'app-food-drink-home',
+    templateUrl: './food-drink-home.component.html',
+    styleUrls: ['./food-drink-home.component.scss']
 })
 export class FoodDrinkHomeComponent implements OnInit {
+    foodDrinkObjects = [];
+    foodDrinkFolder = FOOD_DRINK_FOLDER;
 
-  constructor() { }
+    constructor(
+        private subject: SubjectService,
+        private main: MainService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+        this.changePlace();
+
+        this.subject.getMapData().subscribe(dt => {
+            this.foodDrinkObjects = dt.list;
+        });
+    }
+
+    changePlace() {
+        this.main.changePlace({type: 'food/drink'}).subscribe((dt: any) => {
+            this.foodDrinkObjects = dt;
+        });
+    }
 }
