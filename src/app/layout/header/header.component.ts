7cfdb8a4363ce17e0703c03e0bb6a7721dd33146
Sofia Bruno <sofiabruno19@yahoo.com>
2019-07-30 18:34:09 +0400

    Missing footer files

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Data, NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+    subscriptions: Subscription[] = [];
+    routerUrl = '';
+
+    @Output() toggle = new EventEmitter();
+
+    constructor(
+        public router: Router
+    ) {
+    }
+
+    ngOnInit() {
+        this.subscriptions.push(
+            this.router.events.pipe(
+                filter(event => event instanceof NavigationEnd)
+            ).subscribe((dt: Data) => {
+                this.routerUrl = dt.url;
+            })
+        );
+    }
+
+    toggleSidebar() {
+       this.toggle.emit();
+    }
+
+}
