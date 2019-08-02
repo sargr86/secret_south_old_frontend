163ee0266474d7ea323190441b5c5dc1bac2e105
Sofia Bruno <sofiabruno3003@gmail.com>
2019-08-02 11:29:46 +0400

    Improved accommodation list page, added progress bar, changes checkboxes styles, added range slider

--- src/app/accommodation/accommodations-list/accommodations-list.component.ts
+++ src/app/accommodation/accommodations-list/accommodations-list.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Options} from 'ng5-slider';
+import {MainService} from '../../home/services/main.service';
+import {ACCOMMODATIONS_FOLDER} from '../../shared/constants/settings';
+import {Accommodation} from '../../shared/models/Accommodation';
 
 @Component({
     selector: 'app-accommodations-list',
@@ -6,11 +10,27 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./accommodations-list.component.scss']
 })
 export class AccommodationsListComponent implements OnInit {
+    value = 40;
+    highValue = 180;
+    options: Options = {
+        floor: 0,
+        ceil: 200
+    };
+    accommodationObjects: Accommodation[];
+    accommodationsFolder = ACCOMMODATIONS_FOLDER;
 
-    constructor() {
+    constructor(private main: MainService) {
     }
 
     ngOnInit() {
+        this.getObjects();
+    }
+
+    getObjects() {
+        this.main.changePlace({type: 'accommodations'}).subscribe((dt: any) => {
+
+            this.accommodationObjects = dt;
+        });
     }
 
     getStartDate() {
