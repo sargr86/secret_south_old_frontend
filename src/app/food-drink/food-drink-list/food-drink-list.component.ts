6fd11d4a204a6cfbae3eb6e578f4c0b92a43514d
Sofia Bruno <sofiabruno3003@gmail.com>
2019-08-05 11:54:02 +0400

    Added filters bar to ferries and food-drink sections

--- src/app/food-drink/food-drink-list/food-drink-list.component.ts
+++ src/app/food-drink/food-drink-list/food-drink-list.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MainService} from '../../home/services/main.service';
+import {FoodDrink} from '../../shared/models/FoodDrink';
+import {COUNTRY_RESTRICTED_PLACES} from '../../shared/helpers/google-one-country-places-getter';
+import {TIMEPICKER_THEME} from '../../shared/constants/settings';
 
 @Component({
-  selector: 'app-food-drink-list',
-  templateUrl: './food-drink-list.component.html',
-  styleUrls: ['./food-drink-list.component.scss']
+    selector: 'app-food-drink-list',
+    templateUrl: './food-drink-list.component.html',
+    styleUrls: ['./food-drink-list.component.scss']
 })
 export class FoodDrinkListComponent implements OnInit {
 
-  constructor() { }
+    foodDrinkObjects: FoodDrink[];
+    countryRestrictredPlaces = COUNTRY_RESTRICTED_PLACES;
+    personsCount = 2;
+    timepickerTheme = TIMEPICKER_THEME;
 
-  ngOnInit() {
-  }
 
+    constructor(private main: MainService) {
+    }
+
+    ngOnInit() {
+        this.getObjects();
+    }
+
+    getObjects() {
+        this.main.changePlace({type: 'food-drink'}).subscribe((dt: FoodDrink[]) => {
+            this.foodDrinkObjects = dt;
+        });
+    }
+
+    getStartDate() {
+
+    }
+
+    dateChanged() {
+
+    }
+
+    personsCountChanged(e) {
+
+    }
 }
