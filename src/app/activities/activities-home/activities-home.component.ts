4eee02e2939f9d8a8ef7621971a6f07c05601022
Sofia Bruno <sofiabruno3003@gmail.com>
2019-11-06 13:20:32 +0400

    Added missing headers

--- src/app/activities/activities-home/activities-home.component.ts
+++ src/app/activities/activities-home/activities-home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivitiesService} from '@core/services/activities.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-activities-home',
@@ -9,13 +10,18 @@ import {ActivitiesService} from '@core/services/activities.service';
 export class ActivitiesHomeComponent implements OnInit {
     activityTypes;
     activities;
+    routerUrl;
 
     constructor(
-        private _activities: ActivitiesService
+        private _activities: ActivitiesService,
+        public router: Router
     ) {
     }
 
     ngOnInit() {
+
+        this.routerUrl = this.router.url;
+
         this._activities.getTypes().subscribe(dt => {
             this.activityTypes = dt;
         });
--- src/app/tours/tours-home/tours-home.component.ts
+++ src/app/tours/tours-home/tours-home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-tours-home',
-  templateUrl: './tours-home.component.html',
-  styleUrls: ['./tours-home.component.scss']
+    selector: 'app-tours-home',
+    templateUrl: './tours-home.component.html',
+    styleUrls: ['./tours-home.component.scss']
 })
 export class ToursHomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        public router: Router
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
