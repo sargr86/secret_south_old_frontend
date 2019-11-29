6b3e80c3657ff786e9d19e2ff4410c8fa7ca3a6b
Grigor Sargsyan <sargr86@gmail.com>
2019-11-29 16:41:00 +0400

    Added webpack bundle analyzer, all modules preloading strategy, removed toggle sidebar from couple of places temporarily

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,63 +1,63 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {NotFoundComponent} from '@core/components/not-found/not-found.component';
 import {AuthGuard} from '@core/guards/auth.guard';
 import {RoleGuard} from '@core/guards/role.guard';
 
 const routes: Routes = [
-    {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
-    {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
-    {
-        path: '',
-        redirectTo: 'ferries',
-        pathMatch: 'full'
+  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
+  {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
+  {
+    path: '',
+    redirectTo: 'ferries',
+    pathMatch: 'full'
+  },
+  {
+    path: 'accommodations',
+    loadChildren: './accommodation/accommodation.module#AccommodationModule'
+  },
+  {
+    path: 'activities',
+    loadChildren: './activities/activities.module#ActivitiesModule'
+  },
+  {
+    path: 'food-drink',
+    loadChildren: './food-drink/food-drink.module#FoodDrinkModule'
+  },
+  {
+    path: 'ferries',
+    loadChildren: './ferries/ferries.module#FerriesModule'
+  },
+  {
+    path: 'tours',
+    loadChildren: './tours/tours.module#ToursModule'
+  },
+  {path: '', loadChildren: './home/home.module#HomeModule'},
+  {
+    path: 'partners',
+    data: {
+      expectedRole: 'partner'
     },
-    {
-        path: 'accommodations',
-        loadChildren: './accommodation/accommodation.module#AccommodationModule'
-    },
-    {
-        path: 'activities',
-        loadChildren: './activities/activities.module#ActivitiesModule'
-    },
-    {
-        path: 'food-drink',
-        loadChildren: './food-drink/food-drink.module#FoodDrinkModule'
-    },
-    {
-        path: 'ferries',
-        loadChildren: './ferries/ferries.module#FerriesModule'
-    },
-    {
-        path: 'tours',
-        loadChildren: './tours/tours.module#ToursModule'
-    },
-    {path: '', loadChildren: './home/home.module#HomeModule'},
-    {
-        path: 'partners',
-        data: {
-            expectedRole: 'partner'
-        },
-        canActivate: [AuthGuard, RoleGuard],
-        loadChildren: './partners/partners.module#PartnersModule'
-    },
-    {
-        path: 'employees',
-        canActivate: [AuthGuard],
-        loadChildren: './employees/employees.module#EmployeesModule'
-    },
-    {
-        path: 'customers',
-        canActivate: [AuthGuard],
-        loadChildren: './customers/customers.module#CustomersModule'
-    },
+    canActivate: [AuthGuard, RoleGuard],
+    loadChildren: './partners/partners.module#PartnersModule'
+  },
+  {
+    path: 'employees',
+    canActivate: [AuthGuard],
+    loadChildren: './employees/employees.module#EmployeesModule'
+  },
+  {
+    path: 'customers',
+    canActivate: [AuthGuard],
+    loadChildren: './customers/customers.module#CustomersModule'
+  },
 
-    {path: '**', component: NotFoundComponent},
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
