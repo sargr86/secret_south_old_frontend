6b7103c55166ee23b5908fe38d35844bb32f5187
Grigor Sargsyan <sargr86@gmail.com>
2020-01-14 11:59:08 +0400

    Added Google auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,87 +8,91 @@ import {EMAIL_PATTERN} from '@core/constants/patterns';
 
 import * as jwtDecode from 'jwt-decode';
 import {CommonService} from '@core/services/common.service';
-import {SPINNER_DIAMETER, USER_TYPES} from '@core/constants/settings';
+import {API_URL, SPINNER_DIAMETER, USER_TYPES} from '@core/constants/settings';
 import {Subscription} from 'rxjs';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-    loginForm: FormGroup;
-    userType: string;
-    spinnerDiameter = SPINNER_DIAMETER;
+  loginForm: FormGroup;
+  userType: string;
+  spinnerDiameter = SPINNER_DIAMETER;
 
-    subscriptions: Subscription[] = [];
+  subscriptions: Subscription[] = [];
 
-    constructor(
-        public _router: Router,
-        public _auth: AuthService,
-        private _fb: FormBuilder,
-        public route: ActivatedRoute,
-        public common: CommonService
-    ) {
-    }
+  constructor(
+    public _router: Router,
+    public _auth: AuthService,
+    private _fb: FormBuilder,
+    public route: ActivatedRoute,
+    public common: CommonService
+  ) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.subscriptions.push(this.route.data.subscribe(dt => {
-            this.userType = dt['user'];
-        }));
+    this.subscriptions.push(this.route.data.subscribe(dt => {
+      this.userType = dt['user'];
+    }));
 
-        // Defining login form fields
-        this.loginForm = this._fb.group({
-            email: new FormControl(null, {
-                validators: [Validators.required, patternValidator(EMAIL_PATTERN)]
-            }),
-            password: ['', Validators.required],
-        });
-    }
+    // Defining login form fields
+    this.loginForm = this._fb.group({
+      email: new FormControl(null, {
+        validators: [Validators.required, patternValidator(EMAIL_PATTERN)]
+      }),
+      password: ['', Validators.required],
+    });
+  }
 
-    /**
-     * Authenticate user
-     */
-    login() {
-        this.common.formProcessing = true;
+  /**
+   * Authenticate user
+   */
+  login() {
+    this.common.formProcessing = true;
 
-        this.subscriptions.push(this._auth.login(this.loginForm.value).subscribe(dt => {
+    this.subscriptions.push(this._auth.login(this.loginForm.value).subscribe(dt => {
 
-            // Saving token to browser local storage
-            localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
+      // Saving token to browser local storage
+      localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
 
-            // Gets current user data
-            this._auth.userData = jwtDecode(localStorage.getItem('token'));
+      // Gets current user data
+      this._auth.userData = jwtDecode(localStorage.getItem('token'));
 
-            // Getting redirect url part matching current user role
-            const currentRole = this._auth.userData.role.name_en.toLowerCase();
-            const userType = USER_TYPES.find(d => d.role === currentRole);
+      // Getting redirect url part matching current user role
+      const currentRole = this._auth.userData.role.name_en.toLowerCase();
+      const userType = USER_TYPES.find(d => d.role === currentRole);
 
-            // Navigate to the dashboard page
-            this._router.navigate([`${userType ? userType.label : 'admin'}/dashboard/show`]);
+      // Navigate to the dashboard page
+      this._router.navigate([`${userType ? userType.label : 'admin'}/dashboard/show`]);
 
-        }));
-    }
+    }));
+  }
 
-    /**
-     * E-mail field getter
-     */
-    get email(): AbstractControl {
-        return this.loginForm.get('email');
-    }
+  getGoogleAuthUrl() {
+    return `${API_URL}auth/google`;
+  }
 
-    /**
-     * Password field getter
-     */
-    get pass(): AbstractControl {
-        return this.loginForm.get('password');
-    }
+  /**
+   * E-mail field getter
+   */
+  get email(): AbstractControl {
+    return this.loginForm.get('email');
+  }
 
-    ngOnDestroy() {
-        this.common.formProcessing = false;
-        this.subscriptions.forEach(s => s.unsubscribe());
-    }
+  /**
+   * Password field getter
+   */
+  get pass(): AbstractControl {
+    return this.loginForm.get('password');
+  }
+
+  ngOnDestroy() {
+    this.common.formProcessing = false;
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 
 
 }
--- src/app/ferries/ferries-home/ferries-home.component.ts
+++ src/app/ferries/ferries-home/ferries-home.component.ts
@@ -7,116 +7,125 @@ import {FerryService} from '@core/services/ferry.service';
 import {Ferry} from '@shared/models/Ferry';
 import {NgxGalleryOptions} from 'ngx-gallery';
 import {AuthService} from '@core/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import * as jwtDecode from 'jwt-decode';
 
 @Component({
-    selector: 'app-ferries-home',
-    templateUrl: './ferries-home.component.html',
-    styleUrls: ['./ferries-home.component.scss']
+  selector: 'app-ferries-home',
+  templateUrl: './ferries-home.component.html',
+  styleUrls: ['./ferries-home.component.scss']
 })
 export class FerriesHomeComponent implements OnInit {
 
-    lat = 51.797999;
-    lng = -8.294371;
-    latlng = [];
-    ferryPositions: any = {lat: 0, lng: 0};
-    mapStyles;
-    host = API_URL;
-    selectedFerry = null;
-    selectAction;
-    showFilters = true;
-    ferryData: Ferry;
-    previous;
+  lat = 51.797999;
+  lng = -8.294371;
+  latlng = [];
+  ferryPositions: any = {lat: 0, lng: 0};
+  mapStyles;
+  host = API_URL;
+  selectedFerry = null;
+  selectAction;
+  showFilters = true;
+  ferryData: Ferry;
+  previous;
 
-    galleryOptions: NgxGalleryOptions[] = [
-        {
-            'previewFullscreen': true,
-            'height': '200px',
-            'previewKeyboardNavigation': true,
-            'imageDescription': true,
-            'previewCloseOnEsc': true
-        },
-        {'breakpoint': 500, 'width': '300px', 'height': '300px', 'thumbnailsColumns': 3},
-        {'breakpoint': 300, 'width': '100%', 'height': '200px', 'thumbnailsColumns': 2},
+  galleryOptions: NgxGalleryOptions[] = [
+    {
+      'previewFullscreen': true,
+      'height': '200px',
+      'previewKeyboardNavigation': true,
+      'imageDescription': true,
+      'previewCloseOnEsc': true
+    },
+    {'breakpoint': 500, 'width': '300px', 'height': '300px', 'thumbnailsColumns': 3},
+    {'breakpoint': 300, 'width': '100%', 'height': '200px', 'thumbnailsColumns': 2},
 
-    ];
+  ];
 
-    constructor(
-        private main: MainService,
-        private toastr: ToastrService,
-        private _ferries: FerryService,
-        public auth: AuthService,
-        public router: Router
-    ) {
+  constructor(
+    private main: MainService,
+    private toastr: ToastrService,
+    private _ferries: FerryService,
+    public auth: AuthService,
+    public router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getFerryLocation();
+    this.mapStyles = mapStylesData['default'];
+    this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
+
+    // Saving social auth access token to local storage
+    const token = this.route.snapshot.queryParams.token;
+    console.log(jwtDecode(token))
+    if (token) {
+      localStorage.setItem('token', token);
     }
+  }
 
-    ngOnInit(): void {
-        this.getFerryLocation();
-        this.mapStyles = mapStylesData['default'];
-        this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
-    }
+  getFerryLocation() {
 
-    getFerryLocation() {
+    this.main.getFerryLocation().subscribe((r: any) => {
+      if (r.status === 0) {
+        this.toastr.error(r.message);
+      }
 
-        this.main.getFerryLocation().subscribe((r: any) => {
-            if (r.status === 0) {
-                this.toastr.error(r.message);
-            }
-
-            const arr = [];
-            if (r) {
-                r.map((latlngs) => {
-                    this.ferryPositions.lat = parseFloat(latlngs.lat);
-                    this.ferryPositions.lng = parseFloat(latlngs.lng);
-                    arr.push(latlngs);
-                });
-
-                this.ferryPositions = arr;
-            }
+      const arr = [];
+      if (r) {
+        r.map((latlngs) => {
+          this.ferryPositions.lat = parseFloat(latlngs.lat);
+          this.ferryPositions.lng = parseFloat(latlngs.lng);
+          arr.push(latlngs);
         });
+
+        this.ferryPositions = arr;
+      }
+    });
+  }
+
+  mapClick(e) {
+
+  }
+
+  markerClick(infowindow) {
+    if (this.previous) {
+      this.previous.close();
     }
+    this.previous = infowindow;
+    this.selectedFerry = null;
+    this.selectAction = 'Select';
+  }
 
-    mapClick(e) {
+  closeInfoWindow() {
+    this.selectedFerry = null;
+    this.selectAction = 'Select';
+  }
 
-    }
+  // toggleInfoWindow(ferry) {
+  //     this.selectedFerry = this.selectAction === 'Select' ? ferry : null;
+  //     this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
+  //     this._ferries.getOneFerry({id: ferry.id}).subscribe(d => {
+  //         this.ferryData = d;
+  //     });
+  // }
 
-    markerClick(infowindow) {
-        if (this.previous) {
-            this.previous.close();
-        }
-        this.previous = infowindow;
-        this.selectedFerry = null;
-        this.selectAction = 'Select';
-    }
+  getIcon() {
+    return 'assets/icons/ferry.svg';
+  }
 
-    closeInfoWindow() {
-        this.selectedFerry = null;
-        this.selectAction = 'Select';
-    }
+  selectFerry(ferry) {
 
-    // toggleInfoWindow(ferry) {
-    //     this.selectedFerry = this.selectAction === 'Select' ? ferry : null;
-    //     this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
-    //     this._ferries.getOneFerry({id: ferry.id}).subscribe(d => {
-    //         this.ferryData = d;
-    //     });
-    // }
+    this.selectedFerry = this.selectAction === 'Select' ? ferry : null;
+    this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
+    this._ferries.getOneFerry({id: ferry.id}).subscribe(d => {
+      this.ferryData = d;
+    });
+  }
 
-    getIcon() {
-        return 'assets/icons/ferry.svg';
-    }
-
-    selectFerry(ferry) {
-
-        this.selectedFerry = this.selectAction === 'Select' ? ferry : null;
-        this.selectAction = this.selectedFerry ? 'Cancel' : 'Select';
-        this._ferries.getOneFerry({id: ferry.id}).subscribe(d => {
-            this.ferryData = d;
-        });
-    }
-
-    toggleFilters() {
-        this.showFilters = !this.showFilters;
-    }
+  toggleFilters() {
+    this.showFilters = !this.showFilters;
+  }
 
 }
