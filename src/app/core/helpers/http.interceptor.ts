f6e5a39015c002476660a53a541472214ec80729
Sofia Bruno <sofiabruno19@yahoo.com>
2020-01-16 10:34:01 +0400

    Added 304 status error message disabling

--- src/app/core/helpers/http.interceptor.ts
+++ src/app/core/helpers/http.interceptor.ts
@@ -2,12 +2,12 @@ import {tap} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HttpResponse,
-    HttpErrorResponse,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
 } from '@angular/common/http';
 
 import {Observable} from 'rxjs';
@@ -18,40 +18,40 @@ import {CommonService} from '../services/common.service';
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
 
-    constructor(public router: Router,
-                public toastr: ToastrService,
-                private common: CommonService
-    ) {
-    }
+  constructor(public router: Router,
+              public toastr: ToastrService,
+              private common: CommonService
+  ) {
+  }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(tap((res: HttpResponse<any>) => {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(tap((res: HttpResponse<any>) => {
 
-        }, (err: any) => {
-            if (err instanceof HttpErrorResponse) {
-                this.common.formProcessing = false;
-                this.common.dataLoading = false;
-                const message = err.error.message;
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        this.common.formProcessing = false;
+        this.common.dataLoading = false;
+        const message = err.error.message;
 
-                // if (!this.common.showOverlay) {
-                // Sequelize db connection error
-                if (err.error.hasOwnProperty('db_error')) {
-                    this.toastr.error(err.error.db_error, 'Unable to connect to database');
+        // if (!this.common.showOverlay) {
+        // Sequelize db connection error
+        if (err.error.hasOwnProperty('db_error')) {
+          this.toastr.error(err.error.db_error, 'Unable to connect to database');
 
-                    // Server connection error
-                } else if (err.status === 200 || err.status === 0 || err.error.hasOwnProperty('conn_error')) {
-                    this.toastr.error('Please check server connection.', 'Unable to connect to server');
-                } else {
-                    if (err.error.hasOwnProperty('msg')) {
-                        this.toastr.error('', err.error.msg);
-                    } else if (message) {
-                        this.toastr.error(message.replace(/<(.|\n)*?>/g, ''));
-                    }
-                }
-                // }
+          // Server connection error
+        } else if (err.status === 200 || err.status === 0 || err.error.hasOwnProperty('conn_error') || err.status !== 304) {
+          this.toastr.error('Please check server connection.', 'Unable to connect to server');
+        } else {
+          if (err.error.hasOwnProperty('msg')) {
+            this.toastr.error('', err.error.msg);
+          } else if (message) {
+            this.toastr.error(message.replace(/<(.|\n)*?>/g, ''));
+          }
+        }
+        // }
 
 
-            }
-        }));
-    }
+      }
+    }));
+  }
 }
