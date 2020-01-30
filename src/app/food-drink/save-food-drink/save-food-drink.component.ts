dcbc7c50c27fb6d08556a9d6e9c9ede2cf619c41
Grigor Sargsyan <sargr86@gmail.com>
2020-01-30 17:27:36 +0400

    Fix for food-drink files variable, food-drink routes, change detection fix attempt for sidebar

--- src/app/food-drink/save-food-drink/save-food-drink.component.ts
+++ src/app/food-drink/save-food-drink/save-food-drink.component.ts
@@ -17,162 +17,163 @@ import {ShowFormMessagePipe} from '@shared/pipes/show-form-message.pipe';
 import {BuildFormDataPipe} from '@shared/pipes/build-form-data.pipe';
 
 @Component({
-    selector: 'app-save-food-drink',
-    templateUrl: './save-food-drink.component.html',
-    styleUrls: ['./save-food-drink.component.scss']
+  selector: 'app-save-food-drink',
+  templateUrl: './save-food-drink.component.html',
+  styleUrls: ['./save-food-drink.component.scss']
 })
 export class SaveFoodDrinkComponent implements OnInit, OnDestroy {
-    foodDrinkForm: FormGroup;
-    spinnerDiameter = SPINNER_DIAMETER;
-    foodDrinkData: FoodDrink;
-    formFields = FOOD_DRINK_FIELDS;
-    companies: Company[] = [];
-    redirectUrl = this.getRedirectUrl.transform('food-drink');
-    subscriptions: Subscription[] = [];
-    editCase = false;
-    formAction: string;
-    dropZoneFile: File;
-    dropzoneConfig = {
-        maxFiles: 10
-    };
-    imgPath;
+  foodDrinkForm: FormGroup;
+  spinnerDiameter = SPINNER_DIAMETER;
+  foodDrinkData: FoodDrink;
+  formFields = FOOD_DRINK_FIELDS;
+  companies: Company[] = [];
+  redirectUrl = this.getRedirectUrl.transform('food-drink');
+  subscriptions: Subscription[] = [];
+  editCase = false;
+  formAction: string;
+  dropZoneFiles = [];
+  dropzoneConfig = {
+    maxFiles: 10
+  };
+  imgPath;
 
-    // Address search
-    @ViewChild('searchAddress')
-    public searchElementRef: ElementRef;
-    options = {types: ['geocode']};
+  // Address search
+  @ViewChild('searchAddress')
+  public searchElementRef: ElementRef;
+  options = {types: ['geocode']};
 
 
-    constructor(
-        private _fb: FormBuilder,
-        private _foodDrink: FoodDrinkService,
-        public common: CommonService,
-        public router: Router,
-        private toastr: ToastrService,
-        private route: ActivatedRoute,
-        private checkFormData: CheckFormDataPipe,
-        private _companies: CompaniesService,
-        public auth: AuthService,
-        private getRedirectUrl: RedirectUrlGeneratorPipe,
-        private _formMsg: ShowFormMessagePipe,
-        private formData: BuildFormDataPipe
-    ) {
+  constructor(
+    private _fb: FormBuilder,
+    private _foodDrink: FoodDrinkService,
+    public common: CommonService,
+    public router: Router,
+    private toastr: ToastrService,
+    private route: ActivatedRoute,
+    private checkFormData: CheckFormDataPipe,
+    private _companies: CompaniesService,
+    public auth: AuthService,
+    private getRedirectUrl: RedirectUrlGeneratorPipe,
+    private _formMsg: ShowFormMessagePipe,
+    private formData: BuildFormDataPipe
+  ) {
 
-        this.foodDrinkForm = this._fb.group(this.formFields);
-        this.common.dataLoading = true;
-        this.subscriptions.push(this.route.data.subscribe((dt: Data) => {
-            this.getCompanies();
-            this.prepareEditForm(dt);
+    this.foodDrinkForm = this._fb.group(this.formFields);
+    this.common.dataLoading = true;
+    this.subscriptions.push(this.route.data.subscribe((dt: Data) => {
+      this.getCompanies();
+      this.prepareEditForm(dt);
 
-            this.formAction = this.editCase ? 'update' : 'add';
-            this.common.dataLoading = false;
-        }));
+      this.formAction = this.editCase ? 'update' : 'add';
+      this.common.dataLoading = false;
+    }));
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * Prepares edit form fields & data
+   * @param dt router data
+   */
+  prepareEditForm(dt): void {
+    if (this.route.snapshot.paramMap.get('id')) {
+      this.foodDrinkData = dt['foodDrink'];
+      this.formFields['id'] = '';
+      this.foodDrinkForm = this._fb.group(this.formFields);
+      this.foodDrinkForm.patchValue(this.foodDrinkData);
+      this.editCase = true;
+      this.addressCtrl.disable();
+      if (this.foodDrinkData['img']) {
+        this.imgPath =  this.foodDrinkData['realFolder'] + '/' + + this.foodDrinkData['img'];
+      }
     }
+  }
 
-    ngOnInit() {
+  /**
+   * Resets address and reloads maps api to allow user to select from drop down again
+   */
+  resetAddress() {
+    this.foodDrinkForm.patchValue({'address': ''});
+    this.addressCtrl.enable();
+  }
+
+  /**
+   * Gets activity provider companies list
+   */
+  getCompanies() {
+    this.subscriptions.push(this._companies.get({name: 'food/drink'}).subscribe((dt: Company[]) => {
+      this.companies = dt;
+      this.checkFormData.transform('food/drink', this.foodDrinkData, this.companies, this.editCase);
+    }));
+
+    if (this.auth.checkRoles('partner')) {
+      this.foodDrinkForm.patchValue({company_id: this.auth.userData.company.id})
     }
+  }
 
-    /**
-     * Prepares edit form fields & data
-     * @param dt router data
-     */
-    prepareEditForm(dt): void {
-        if (this.route.snapshot.paramMap.get('id')) {
-            this.foodDrinkData = dt['foodDrink'];
-            this.formFields['id'] = '';
-            this.foodDrinkForm = this._fb.group(this.formFields);
-            this.foodDrinkForm.patchValue(this.foodDrinkData);
-            this.editCase = true;
-            this.addressCtrl.disable();
-            if (this.foodDrinkData['img']) {
-                this.imgPath = FOOD_DRINK_FOLDER + this.foodDrinkData['img'];
-            }
-        }
-    }
+  /**
+   * Adds/updates food drink info
+   * @param address food-drink address
+   */
+  save(address) {
 
-    /**
-     * Resets address and reloads maps api to allow user to select from drop down again
-     */
-    resetAddress() {
-        this.foodDrinkForm.patchValue({'address': ''});
-        this.addressCtrl.enable();
-    }
+console.log(this.foodDrinkForm.value)
+    // if (this.foodDrinkForm.valid) {
+    this.common.formProcessing = true;
+    const formData = this.formData.transform({
+      ...this.foodDrinkForm.value,
+      address: address.el.nativeElement.value
+    }, this.dropZoneFiles);
 
-    /**
-     * Gets activity provider companies list
-     */
-    getCompanies() {
-        this.subscriptions.push(this._companies.get({name: 'food/drink'}).subscribe((dt: Company[]) => {
-            this.companies = dt;
-            this.checkFormData.transform('food/drink', this.foodDrinkData, this.companies, this.editCase);
-        }));
-
-        if (this.auth.checkRoles('partner')) {
-            this.foodDrinkForm.patchValue({company_id: this.auth.userData.company.id})
-        }
-    }
-
-    /**
-     * Adds/updates food drink info
-     * @param address food-drink address
-     */
-    save(address) {
-
-        // if (this.foodDrinkForm.valid) {
-        this.common.formProcessing = true;
-        const formData = this.formData.transform({
-            ...this.foodDrinkForm.value,
-            address: address.el.nativeElement.value
-        }, this.dropZoneFile);
-
-        this.subscriptions.push(this._foodDrink[this.formAction](formData).subscribe(() => {
-            this._formMsg.transform('food/drink', this.editCase, this.redirectUrl);
-        }));
+    this.subscriptions.push(this._foodDrink[this.formAction](formData).subscribe(() => {
+      this._formMsg.transform('food/drink', this.editCase, this.redirectUrl);
+    }));
 
 
-        // }
-    }
+    // }
+  }
 
-    /**
-     * Gets drop zone file
-     * @param e the file
-     */
-    getFile(e) {
-        this.dropZoneFile = e;
-    }
+  /**
+   * Gets drop zone file
+   * @param e the file
+   */
+  getFile(e) {
+    this.dropZoneFiles.push(e);
+  }
 
 
-    /**
-     * Removes saved drop zone image
-     */
-    removeSavedImg() {
-        this.imgPath = '';
-        this.foodDrinkForm.patchValue({'img': ''});
-        this.dropZoneFile = null;
-    }
+  /**
+   * Removes saved drop zone image
+   */
+  removeSavedImg() {
+    this.imgPath = '';
+    this.foodDrinkForm.patchValue({'img': ''});
+    this.dropZoneFiles = null;
+  }
 
-    get latCtrl() {
-        return this.foodDrinkForm.get('lat');
-    }
+  get latCtrl() {
+    return this.foodDrinkForm.get('lat');
+  }
 
-    get lngCtrl() {
-        return this.foodDrinkForm.get('lng');
-    }
+  get lngCtrl() {
+    return this.foodDrinkForm.get('lng');
+  }
 
-    get addressCtrl() {
-        return this.foodDrinkForm.get('address');
-    }
+  get addressCtrl() {
+    return this.foodDrinkForm.get('address');
+  }
 
-    get descCtrl() {
-        return this.foodDrinkForm.get('description');
-    }
+  get descCtrl() {
+    return this.foodDrinkForm.get('description');
+  }
 
-    get companyCtrl() {
-        return this.foodDrinkForm.get('company_id');
-    }
+  get companyCtrl() {
+    return this.foodDrinkForm.get('company_id');
+  }
 
-    ngOnDestroy() {
-        this.subscriptions.forEach(s => s.unsubscribe());
-    }
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 
 }
