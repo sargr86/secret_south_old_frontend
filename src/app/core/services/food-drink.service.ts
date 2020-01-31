463c8ae50282c97cd2517467b84cf1cd59857180
Sofia Bruno <sofiabruno3003@gmail.com>
2020-01-31 12:18:50 +0400

    Fixes for edit-food-drink form

--- src/app/core/services/food-drink.service.ts
+++ src/app/core/services/food-drink.service.ts
@@ -3,39 +3,43 @@ import {HttpClient} from '@angular/common/http';
 import {API_URL} from '@core/constants/settings';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class FoodDrinkService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    add(data) {
-        return this.http.post(`${API_URL}food-drink/add`, data);
-    }
+  add(data) {
+    return this.http.post(`${API_URL}food-drink/add`, data);
+  }
 
-    insertFoodDrink(data) {
-        return this.http.post(`${API_URL}food-drink/add`, data);
-    }
+  insertFoodDrink(data) {
+    return this.http.post(`${API_URL}food-drink/add`, data);
+  }
 
-    getFoodDrink(params) {
-        return this.http.get(`${API_URL}food-drink/get`, {params: params});
-    }
+  getFoodDrink(params) {
+    return this.http.get(`${API_URL}food-drink/get`, {params: params});
+  }
 
-    getOneFoodDrink(params) {
-        return this.http.get(`${API_URL}food-drink/getOne`, {params: params});
-    }
+  getOneFoodDrink(params) {
+    return this.http.get(`${API_URL}food-drink/getOne`, {params: params});
+  }
 
-    getPartners() {
-        return this.http.get(`${API_URL}food-drink/get-partners`);
-    }
+  getPartners() {
+    return this.http.get(`${API_URL}food-drink/get-partners`);
+  }
+
+  makeCover(params) {
+    return this.http.put(`${API_URL}food-drink/make-cover`, params);
+  }
 
 
-    update(params) {
-        return this.http.put(`${API_URL}food-drink/update`, params);
-    }
+  update(params) {
+    return this.http.put(`${API_URL}food-drink/update`, params);
+  }
 
-    remove(params) {
-        return this.http.delete(`${API_URL}food-drink/remove`, {params: params});
-    }
+  remove(params) {
+    return this.http.delete(`${API_URL}food-drink/remove`, {params: params});
+  }
 }
