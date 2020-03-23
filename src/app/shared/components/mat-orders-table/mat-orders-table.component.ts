bab971695a68488b6261e9de9994f9317a48bbe9
Sofia Bruno <sofiabruno3003@gmail.com>
2020-03-23 08:38:22 +0400

    Added positions model----, assign-driver-to-boat functionality, mat-orders-table component

--- src/app/shared/components/mat-orders-table/mat-orders-table.component.ts
+++ src/app/shared/components/mat-orders-table/mat-orders-table.component.ts
@@ -5,6 +5,10 @@ import {Subscription} from 'rxjs';
 import {ToastrService} from 'ngx-toastr';
 import {Socket} from 'ngx-socket-io';
 import {SubjectService} from '@core/services/subject.service';
+import {Router} from '@angular/router';
+import {AuthService} from '@core/services/auth.service';
+import {MatDialog} from '@angular/material/dialog';
+import {DriverAssignmentDialogComponent} from '@core/components/dialogs/driver-assignment-dialog/driver-assignment-dialog.component';
 
 
 @Component({
@@ -14,7 +18,7 @@ import {SubjectService} from '@core/services/subject.service';
 })
 export class MatOrdersTableComponent implements OnInit, OnDestroy {
   @Input() status;
-  displayedColumns = ['client_full_name', 'phone', 'email', 'start_point', 'end_point', 'time', 'status'];
+  displayedColumns = ['_id', 'client_full_name', 'phone', 'email', 'start_point', 'end_point', 'time', 'status', 'actions'];
   dataSource;
   filteredData;
   subscriptions: Subscription[] = [];
@@ -25,6 +29,9 @@ export class MatOrdersTableComponent implements OnInit, OnDestroy {
     private toastr: ToastrService,
     private subject: SubjectService,
     public socket: Socket,
+    public router: Router,
+    public auth: AuthService,
+    private matDialog: MatDialog
   ) {
   }
 
@@ -32,15 +39,25 @@ export class MatOrdersTableComponent implements OnInit, OnDestroy {
 
     this.getOrders();
 
+    // Getting orders of the changed tab
+    this.subject.getOrderTypeData().subscribe(status => {
+      this.status = status;
+      this.getOrders();
+    });
+
 
     this.socket.on('orderCreated', (data) => {
-      console.log(data)
       const customer = data.order.client;
       if (customer) {
         this.toastr.success(`A new order has been created by: <strong>${customer.first_name}  ${customer.last_name}</strong>`,
           'Order created!', {enableHtml: true});
         this.getOrders();
       }
+    });
+
+    this.socket.on('orderTakenFinished', (data) => {
+      console.log('order taken finished')
+      this.getOrders();
     });
   }
 
@@ -64,6 +81,26 @@ export class MatOrdersTableComponent implements OnInit, OnDestroy {
     }
     this.filteredData = this.dataSource.filteredData;
     // console.log(this.dataSource)
+  }
+
+  assignToDriver(row) {
+    this.matDialog.open(DriverAssignmentDialogComponent, {
+      data: {order_id: row._id},
+      width: '500px',
+      height: '400px'
+    }).afterClosed().subscribe(dt => {
+      const client = row.client;
+      row['driver'] = dt.driver;
+      this.socket.emit('driverAssigned', {driverOrder: dt, selectedOrder: row});
+      this.socket.on('driverAssignmentFinished', (res) => {
+        if (res) {
+          this.toastr.success(`The order of customer <strong>${client.first_name} ${client.last_name}</strong>
+            has been assigned to <strong>${dt.driver.full_name}</strong>`,
+            '', {enableHtml: true})
+          this.getOrders();
+        }
+      });
+    });
   }
 
   ngOnDestroy() {
