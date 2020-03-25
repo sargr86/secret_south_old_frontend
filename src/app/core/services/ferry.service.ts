3e5ae0e63c2d59bba752fa06c6faad4b16d49bff
Grigor Sargsyan <sargr86@gmail.com>
2020-03-25 13:58:39 +0400

    Assigning a ferry to a driver is done, added seeders for ferry companies and one ferry

--- src/app/core/services/ferry.service.ts
+++ src/app/core/services/ferry.service.ts
@@ -4,47 +4,51 @@ import * as Base from '../../config.js';
 import {API_URL} from '../constants/settings';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class FerryService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    getAllpartner() {
-        return this.http.get(API_URL + 'ferries/get-partners');
-    }
+  getAllpartner() {
+    return this.http.get(API_URL + 'ferries/get-partners');
+  }
 
-    add(data) {
+  add(data) {
 
-        return this.http.post(`${API_URL}ferries/add`, data);
-    }
+    return this.http.post(`${API_URL}ferries/add`, data);
+  }
 
-    getFerries(params) {
-        return this.http.get(`${API_URL}ferries/get`, {params: params});
-    }
+  getFerries(params) {
+    return this.http.get(`${API_URL}ferries/get`, {params: params});
+  }
 
-    getPartnerFerries() {
-        return this.http.get(`${API_URL}ferries/get-for-partner`);
-    }
+  getPartnerFerries() {
+    return this.http.get(`${API_URL}ferries/get-for-partner`);
+  }
 
-    getOneFerry(params) {
-        return this.http.get(`${API_URL}ferries/getOne`, {params: params});
-    }
+  getOneFerry(params) {
+    return this.http.get(`${API_URL}ferries/getOne`, {params: params});
+  }
 
-    update(params) {
-        return this.http.put(`${API_URL}ferries/update`, params);
-    }
+  update(params) {
+    return this.http.put(`${API_URL}ferries/update`, params);
+  }
 
-    remove(params) {
-        return this.http.delete(`${API_URL}ferries/remove`, {params: params});
-    }
+  remove(params) {
+    return this.http.delete(`${API_URL}ferries/remove`, {params: params});
+  }
 
-    removeImage(params){
-        return this.http.delete(`${API_URL}ferries/remove-image`, {params: params});
-    }
+  removeImage(params) {
+    return this.http.delete(`${API_URL}ferries/remove-image`, {params: params});
+  }
 
-    makeCover(params) {
-        return this.http.put(`${API_URL}ferries/make-cover`, params);
-    }
+  makeCover(params) {
+    return this.http.put(`${API_URL}ferries/make-cover`, params);
+  }
+
+  assignDriver(params) {
+    return this.http.put(`${API_URL}ferries/assign-driver`, params);
+  }
 }
--- src/app/partners/save-partner/save-partner.component.ts
+++ src/app/partners/save-partner/save-partner.component.ts
@@ -11,114 +11,115 @@ import {PartnerType} from '@shared/models/PartnerType';
 import {Partner} from '@shared/models/Partner';
 
 @Component({
-    selector: 'app-save-partner',
-    templateUrl: './save-partner.component.html',
-    styleUrls: ['./save-partner.component.scss']
+  selector: 'app-save-partner',
+  templateUrl: './save-partner.component.html',
+  styleUrls: ['./save-partner.component.scss']
 })
 export class SavePartnerComponent implements OnInit {
 
-    partnersFields = {
-        first_name: ['', Validators.required],
-        last_name: ['', Validators.required],
-        email: ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
-        partner_type_id: ['', Validators.required],
-        phone: ['', [Validators.required]],
-        id: ''
-    };
-    savePartnerForm: FormGroup;
-    editCase = false;
-    partnerInfo: Partner;
-    spinnerDiameter = SPINNER_DIAMETER;
-    allowedCountries = ALLOWED_COUNTRIES;
-    defaultCountry = DEFAULT_COUNTRY;
-    redirectUrl = 'admin/partners/show';
-    partnerType: PartnerType;
+  partnersFields = {
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    email: ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
+    partner_type_id: ['', Validators.required],
+    phone: ['', [Validators.required]],
+    id: ''
+  };
+  savePartnerForm: FormGroup;
+  editCase = false;
+  partnerInfo: Partner;
+  spinnerDiameter = SPINNER_DIAMETER;
+  allowedCountries = ALLOWED_COUNTRIES;
+  defaultCountry = DEFAULT_COUNTRY;
+  redirectUrl = 'admin/partners/show';
+  partnerType: PartnerType;
 
-    constructor(
-        private _fb: FormBuilder,
-        private route: ActivatedRoute,
-        private _partner: PartnerService,
-        private router: Router,
-        private toastr: ToastrService,
-        public common: CommonService,
-    ) {
-        if (!this.editCase) {
-            this.partnersFields['pass'] = ['', Validators.required];
-        }
+  constructor(
+    private _fb: FormBuilder,
+    private route: ActivatedRoute,
+    private _partner: PartnerService,
+    private router: Router,
+    private toastr: ToastrService,
+    public common: CommonService,
+  ) {
+    if (!this.editCase) {
+      this.partnersFields['pass'] = ['', Validators.required];
+    }
+    this.savePartnerForm = this._fb.group(this.partnersFields);
+
+    this._partner.getTypes().subscribe(d => {
+      this.partnerType = d;
+    });
+
+    const partner_id = this.route.snapshot.paramMap.get('id');
+    if (partner_id) {
+      this.common.dataLoading = true;
+      this.editCase = true;
+
+      this._partner.getOnePartner({id: partner_id}).subscribe(dt => {
+
+        this.partnerInfo = dt;
+        delete this.partnersFields['pass'];
         this.savePartnerForm = this._fb.group(this.partnersFields);
+        this.savePartnerForm.patchValue(this.partnerInfo);
+        this.common.dataLoading = false;
+      });
 
-        this._partner.getTypes().subscribe(d => {
-            this.partnerType = d;
-        });
-
-        const partner_id = this.route.snapshot.paramMap.get('id');
-        if (partner_id) {
-            this.common.dataLoading = true;
-            this.editCase = true;
-
-            this._partner.getOnePartner({id: partner_id}).subscribe(dt => {
-
-                this.partnerInfo = dt;
-                delete this.partnersFields['pass'];
-                this.savePartnerForm = this._fb.group(this.partnersFields);
-                this.savePartnerForm.patchValue(this.partnerInfo);
-                this.common.dataLoading = false;
-            });
-
-
-        }
 
     }
+    this.common.dataLoading = false;
 
-    ngOnInit() {
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * Adds or updates a partner info
+   */
+  savePartner() {
+    const data = this.savePartnerForm.value;
+    this.common.formProcessing = true;
+    if (this.editCase) {
+
+      this._partner.updatePartnerInfo(data).subscribe(() => {
+        this.toastr.success('Partner info has been updated successfully');
+        this.router.navigate([this.redirectUrl]);
+        this.common.formProcessing = false;
+      });
+
+    } else {
+      this._partner.insertPartner(data).subscribe(() => {
+        this.toastr.success('Partner info has been added successfully');
+        this.router.navigate([this.redirectUrl]);
+        this.common.formProcessing = false;
+      });
     }
+  }
 
-    /**
-     * Adds or updates a partner info
-     */
-    savePartner() {
-        const data = this.savePartnerForm.value;
-        this.common.formProcessing = true;
-        if (this.editCase) {
+  get showForm() {
+    return !this.common.dataLoading && (!this.editCase || (this.partnerInfo && this.editCase));
+  }
 
-            this._partner.updatePartnerInfo(data).subscribe(() => {
-                this.toastr.success('Partner info has been updated successfully');
-                this.router.navigate([this.redirectUrl]);
-                this.common.formProcessing = false;
-            });
+  get firstNameCtrl() {
+    return this.savePartnerForm.get('first_name');
+  }
 
-        } else {
-            this._partner.insertPartner(data).subscribe(() => {
-                this.toastr.success('Partner info has been added successfully');
-                this.router.navigate([this.redirectUrl]);
-                this.common.formProcessing = false;
-            });
-        }
-    }
+  get lastNameCtrl() {
+    return this.savePartnerForm.get('last_name');
+  }
 
-    get showForm() {
-        return !this.common.dataLoading && (!this.editCase || (this.partnerInfo && this.editCase));
-    }
+  get emailCtrl() {
+    return this.savePartnerForm.get('email');
+  }
 
-    get firstNameCtrl() {
-        return this.savePartnerForm.get('first_name');
-    }
+  get phoneCtrl() {
+    return this.savePartnerForm.get('phone');
+  }
 
-    get lastNameCtrl() {
-        return this.savePartnerForm.get('last_name');
-    }
-
-    get emailCtrl() {
-        return this.savePartnerForm.get('email');
-    }
-
-    get phoneCtrl() {
-        return this.savePartnerForm.get('phone');
-    }
-
-    changed(e) {
-        this.savePartnerForm.patchValue({'phone': e.target.value});
-    }
+  changed(e) {
+    this.savePartnerForm.patchValue({'phone': e.target.value});
+  }
 
 
 }
