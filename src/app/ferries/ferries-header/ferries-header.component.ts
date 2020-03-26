e246d60103edfa2625974ab107ca4398773fb6e9
Sofia Bruno <sofiabruno19@yahoo.com>
2020-03-26 12:16:39 +0400

    Added time picker input, prepared the styles for order system

--- src/app/ferries/ferries-header/ferries-header.component.ts
+++ src/app/ferries/ferries-header/ferries-header.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {MAIN_SECTIONS} from '@core/constants/settings';
+import {MAIN_SECTIONS, TIMEPICKER_THEME} from '@core/constants/settings';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {COUNTRY_RESTRICTED_PLACES} from '@core/helpers/google-one-country-places-getter';
@@ -10,111 +10,112 @@ import {SubjectService} from '@core/services/subject.service';
 import IsResponsive from '@core/helpers/is-responsive';
 
 @Component({
-    selector: 'app-ferries-header',
-    templateUrl: './ferries-header.component.html',
-    styleUrls: ['./ferries-header.component.scss']
+  selector: 'app-ferries-header',
+  templateUrl: './ferries-header.component.html',
+  styleUrls: ['./ferries-header.component.scss']
 })
 export class FerriesHeaderComponent implements OnInit {
 
-    mainSections = MAIN_SECTIONS;
-    mapForm: FormGroup;
-    latlng: any = [];
-    lat = 0;
-    lng = 0;
-    subscriptions: Subscription[] = [];
-    routerUrl: string;
-    selectedSection = 'Ferries';
-    responsiveMode: boolean;
-    countryRestrictedPlaces = COUNTRY_RESTRICTED_PLACES as any;
-    personsCount = 2;
+  mainSections = MAIN_SECTIONS;
+  mapForm: FormGroup;
+  latlng: any = [];
+  lat = 0;
+  lng = 0;
+  subscriptions: Subscription[] = [];
+  routerUrl: string;
+  selectedSection = 'Ferries';
+  responsiveMode: boolean;
+  countryRestrictedPlaces = COUNTRY_RESTRICTED_PLACES as any;
+  personsCount = 2;
+  timepickerTheme = TIMEPICKER_THEME;
 
-    @Output() toggle = new EventEmitter();
+  @Output() toggle = new EventEmitter();
 
-    constructor(
-        public router: Router,
-        public auth: AuthService,
-        private _fb: FormBuilder,
-        private main: MainService,
-        private subject: SubjectService
-    ) {
+  constructor(
+    public router: Router,
+    public auth: AuthService,
+    private _fb: FormBuilder,
+    private main: MainService,
+    private subject: SubjectService
+  ) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        // Checking for responsive mode and initializing map form
-        this.responsiveMode = IsResponsive.check();
-        this.mapForm = this._fb.group({
-            type: ['']
+    // Checking for responsive mode and initializing map form
+    this.responsiveMode = IsResponsive.check();
+    this.mapForm = this._fb.group({
+      type: ['']
+    });
+  }
+
+  toggleSidebar() {
+    this.subject.setSidebarAction('toggle');
+    this.toggle.emit();
+  }
+
+  changePlace(section) {
+    this.main.changePlace(this.mapForm.value).subscribe((r: any) => {
+
+      this.latlng = [];
+
+      if (r && r.length > 0) {
+
+        r.map((latlngs) => {
+          latlngs.lat = parseFloat(latlngs.lat);
+          latlngs.lng = parseFloat(latlngs.lng);
+          this.latlng.push(latlngs);
         });
+
+        this.lat = parseFloat(this.latlng[0].lat);
+        this.lng = parseFloat(this.latlng[0].lng);
+
+
+      }
+
+      this.subject.setMapData({
+        section: section,
+        lat: this.lat,
+        lng: this.lng,
+        list: r
+      });
+      this.selectedSection = section;
+    });
+  }
+
+  changeSection(section) {
+    if (section === 'Accommodations') {
+      this.mapForm.patchValue({type: section});
+      this.router.navigate([section.toLowerCase()]);
+      this.changePlace(section);
     }
+  }
 
-    toggleSidebar() {
-      this.subject.setSidebarAction('toggle');
-        this.toggle.emit();
-    }
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+  }
 
-    changePlace(section) {
-        this.main.changePlace(this.mapForm.value).subscribe((r: any) => {
+  navigateToDashboard() {
+    const role = this.auth.checkRoles('admin') ? 'admin' : (this.auth.checkRoles('partner') ? 'partners' : 'employees');
+    this.router.navigate([`${role}/dashboard/show`]);
+  }
 
-            this.latlng = [];
+  getStartDate() {
 
-            if (r && r.length > 0) {
+  }
 
-                r.map((latlngs) => {
-                    latlngs.lat = parseFloat(latlngs.lat);
-                    latlngs.lng = parseFloat(latlngs.lng);
-                    this.latlng.push(latlngs);
-                });
+  dateChanged() {
 
-                this.lat = parseFloat(this.latlng[0].lat);
-                this.lng = parseFloat(this.latlng[0].lng);
+  }
 
+  searchFerries() {
+    // this.router.navigate(['ferries/list']);
+  }
 
-            }
+  personsCountChanged(e) {
 
-            this.subject.setMapData({
-                section: section,
-                lat: this.lat,
-                lng: this.lng,
-                list: r
-            });
-            this.selectedSection = section;
-        });
-    }
-
-    changeSection(section) {
-        if (section === 'Accommodations') {
-            this.mapForm.patchValue({type: section});
-            this.router.navigate([section.toLowerCase()]);
-            this.changePlace(section);
-        }
-    }
-
-    logout() {
-        localStorage.removeItem('token');
-        this.router.navigate(['/']);
-    }
-
-    navigateToDashboard() {
-        const role = this.auth.checkRoles('admin') ? 'admin' : (this.auth.checkRoles('partner') ? 'partners' : 'employees');
-        this.router.navigate([`${role}/dashboard/show`]);
-    }
-
-    getStartDate() {
-
-    }
-
-    dateChanged() {
-
-    }
-
-    searchFerries() {
-        // this.router.navigate(['ferries/list']);
-    }
-
-    personsCountChanged(e) {
-
-    }
+  }
 
 }
