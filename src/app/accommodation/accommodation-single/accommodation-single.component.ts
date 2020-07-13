6d860c27aa81e59cb43a7a790ebb41de815ef622
Grigor Sargsyan <sargr86@gmail.com>
2020-07-13 11:54:48 +0400

    Updated angular version to 9.1, fixed slider on single pages

--- src/app/accommodation/accommodation-single/accommodation-single.component.ts
+++ src/app/accommodation/accommodation-single/accommodation-single.component.ts
@@ -1,29 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {AccommodationsService} from '@core/services/accommodations.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {CommonService} from '@core/services/common.service';
 
 @Component({
-    selector: 'app-accommodation-single',
-    templateUrl: './accommodation-single.component.html',
-    styleUrls: ['./accommodation-single.component.scss']
+  selector: 'app-accommodation-single',
+  templateUrl: './accommodation-single.component.html',
+  styleUrls: ['./accommodation-single.component.scss']
 })
 export class AccommodationSingleComponent implements OnInit {
-    accommodation;
+  accommodation;
 
-    constructor(
-        private _accommodations: AccommodationsService,
-        private router: Router,
-        private route: ActivatedRoute
-    ) {
-    }
+  constructor(
+    private _accommodations: AccommodationsService,
+    private router: Router,
+    private route: ActivatedRoute,
+    public common: CommonService
+  ) {
+    this.common.dataLoading = false;
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        const accommodation_id = this.route.snapshot.params.id;
+    const accommodation_id = this.route.snapshot.params.id;
 
-        this._accommodations.getOne({id: accommodation_id}).subscribe(dt => {
-            this.accommodation = dt;
-        });
-    }
+    this._accommodations.getOne({id: accommodation_id}).subscribe(dt => {
+      this.accommodation = dt;
+    });
+  }
 
 }
