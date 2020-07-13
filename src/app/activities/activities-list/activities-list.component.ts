0dbe1b21032dadf32e0c9354ce875c8e9ed04a45
Sofia Bruno <sofiabruno3003@gmail.com>
2020-07-13 14:40:57 +0400

    Showing selected accommodation/activity provider/food-drink object locations on the map, fixed nav bar issue for the each of listed objects single pages

--- src/app/activities/activities-list/activities-list.component.ts
+++ src/app/activities/activities-list/activities-list.component.ts
@@ -2,45 +2,48 @@ import {Component, OnInit} from '@angular/core';
 import {MainService} from '@core/services/main.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ACTIVITIES_FOLDER, TIMEPICKER_THEME} from '@core/constants/global';
+import {CommonService} from '@core/services/common.service';
 
 @Component({
-    selector: 'app-activities-list',
-    templateUrl: './activities-list.component.html',
-    styleUrls: ['./activities-list.component.scss']
+  selector: 'app-activities-list',
+  templateUrl: './activities-list.component.html',
+  styleUrls: ['./activities-list.component.scss']
 })
 export class ActivitiesListComponent implements OnInit {
-    activityProviders;
-    activityFolder = ACTIVITIES_FOLDER;
-    personsCount = 2;
-    timepickerTheme = TIMEPICKER_THEME;
+  activityProviders;
+  activityFolder = ACTIVITIES_FOLDER;
+  personsCount = 2;
+  timepickerTheme = TIMEPICKER_THEME;
 
-    constructor(
-        private main: MainService,
-        public router: Router,
-        private route: ActivatedRoute
-    ) {
-    }
+  constructor(
+    private main: MainService,
+    public router: Router,
+    private route: ActivatedRoute,
+    public common: CommonService
+  ) {
+    this.common.dataLoading = false;
+  }
 
-    ngOnInit() {
-        this.getObjects();
-    }
+  ngOnInit() {
+    this.getObjects();
+  }
 
-    getObjects() {
-        this.main.changePlace({type: 'activities'}).subscribe((dt) => {
-            this.activityProviders = dt;
-        });
-    }
+  getObjects() {
+    this.main.changePlace({type: 'activities'}).subscribe((dt) => {
+      this.activityProviders = dt;
+    });
+  }
 
-    getStartDate() {
+  getStartDate() {
 
-    }
+  }
 
-    dateChanged() {
+  dateChanged() {
 
-    }
+  }
 
-    personsCountChanged(e) {
+  personsCountChanged(e) {
 
-    }
+  }
 
 }
--- src/app/food-drink/food-drink-single/food-drink-single.component.ts
+++ src/app/food-drink/food-drink-single/food-drink-single.component.ts
@@ -4,28 +4,30 @@ import {FoodDrinkService} from '@core/services/food-drink.service';
 import {CommonService} from '@core/services/common.service';
 
 @Component({
-    selector: 'app-food-drink-single',
-    templateUrl: './food-drink-single.component.html',
-    styleUrls: ['./food-drink-single.component.scss']
+  selector: 'app-food-drink-single',
+  templateUrl: './food-drink-single.component.html',
+  styleUrls: ['./food-drink-single.component.scss']
 })
 export class FoodDrinkSingleComponent implements OnInit {
-    foodDrinkProvider;
+  foodDrinkProvider;
+  location = 'Cronin\'s Pub';
 
-    constructor(
-        private _food_drink: FoodDrinkService,
-        private router: Router,
-        private route: ActivatedRoute,
-        public common: CommonService
-    ) {
-      this.common.dataLoading = false;
-    }
+  constructor(
+    private _food_drink: FoodDrinkService,
+    private router: Router,
+    private route: ActivatedRoute,
+    public common: CommonService,
+  ) {
+    this.common.dataLoading = false;
 
-    ngOnInit() {
-        const foodDrinkId = this.route.snapshot.params.id;
+  }
 
-        this._food_drink.getOneFoodDrink({id: foodDrinkId}).subscribe(dt => {
-            this.foodDrinkProvider = dt;
-        });
-    }
+  ngOnInit() {
+    const foodDrinkId = this.route.snapshot.params.id;
+
+    this._food_drink.getOneFoodDrink({id: foodDrinkId}).subscribe(dt => {
+      this.foodDrinkProvider = dt;
+    });
+  }
 
 }
