fd8fafceb2dea600fb43757555def1e9c3511441
David Burton <davitburton280@gmail.com>
2020-10-23 16:53:25 +0400

    Save-tour form fixes in dashboard

--- src/app/shared/models/Tour.ts
+++ src/app/shared/models/Tour.ts
@@ -1,11 +1,12 @@
 export interface Tour {
-    id?: number;
-    name?: string;
-    address?: string;
-    lat?: number;
-    lng?: number;
-    partner_id?: number;
-    tours_type_id?: number;
-    type_name?: string;
-    type?: string;
+  id?: number;
+  name?: string;
+  address?: string;
+  lat?: number;
+  lng?: number;
+  partner_id?: number;
+  tours_type_id?: number;
+  type_name?: string;
+  type?: string;
+  images?: any;
 }
