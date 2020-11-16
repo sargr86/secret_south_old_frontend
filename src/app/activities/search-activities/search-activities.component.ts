20b1a3132713843164ea5843e27df12433e33306
Sofia Bruno <sofiabruno3003@gmail.com>
2020-11-16 11:47:00 +0400

    Added mission endpoints for activities

--- src/app/activities/search-activities/search-activities.component.ts
+++ src/app/activities/search-activities/search-activities.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import moment from 'moment';
+import {ActivityTypesService} from '@core/services/activity-types.service';
+import {ActivitiesService} from '@core/services/activities.service';
+import {map, startWith} from 'rxjs/operators';
+import {FilterLocationsForDropdownPipe} from '@shared/pipes/filter-locations-for-dropdown.pipe';
 
 @Component({
   selector: 'app-search-activities',
@@ -14,36 +18,100 @@ export class SearchActivitiesComponent implements OnInit {
   adultsCount = 2;
   childrenCount = 2;
 
+  activityProviders;
+  activityTypes;
+  activitySubTypes;
+  filteredLocations;
+  locationControl = new FormControl();
+
   previousDatesFilter = (d: Date | null): boolean => {
     return moment(d).isSameOrAfter(moment(), 'day');
   }
 
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private activityTypesService: ActivityTypesService,
+    private activitiesService: ActivitiesService,
+    private filterLocations: FilterLocationsForDropdownPipe
+  ) {
     this.activitiesForm = this.fb.group({
+      location: [''],
       date: [''],
-      children: [''],
-      adults: ['']
+      type: [''],
+      children: [this.childrenCount],
+      adults: [this.adultsCount]
     });
   }
 
   ngOnInit(): void {
+
+    this.getActivityProviders();
+    this.getActivityTypes();
+    this.getSubtypes();
+
   }
 
-  dateChanged(e) {
+  getActivityProviders() {
+    this.activitiesService.get().subscribe((d: any) => {
+      this.activityProviders = d;
 
+      this.filteredLocations = this.locationControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterLocations.transform(value, d))
+      );
+    });
+  }
+
+  getActivityTypes() {
+    this.activitiesService.getTypes().subscribe(dt => {
+      this.activityTypes = dt;
+    });
+  }
+
+  getSubtypes() {
+    this.activityTypesService.getSubtypes({type_id: this.activityType.value}).subscribe((dt) => {
+      this.activitySubTypes = dt;
+    });
+  }
+
+  clearLocation() {
+    this.locationControl.patchValue('');
+    this.activitiesForm.patchValue({location: ''});
+    this.getActivityProviders();
+  }
+
+
+  dateChanged(e) {
+    console.log(e.value)
+    this.activitiesForm.patchValue({date: e.value});
   }
 
   adultsCountChanged(e) {
-
+    this.activitiesForm.patchValue({adults: e});
   }
 
   childrenCountChanged(e) {
+    this.activitiesForm.patchValue({children: e});
+  }
 
+  locationChanged(e) {
+    this.activitiesForm.patchValue({location: e});
+  }
+
+  typeChanged(e) {
+    this.activitiesForm.patchValue({type: e.target.value});
+    console.log(this.activityType.value)
+    this.getSubtypes();
   }
 
   search() {
-
+    console.log(this.activitiesForm.value)
   }
+
+  get activityType(): AbstractControl {
+    return this.activitiesForm.get('type');
+  }
+
 
 }
