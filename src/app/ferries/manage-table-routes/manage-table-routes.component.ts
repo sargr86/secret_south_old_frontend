0fda45af9edbb35d73ca22df9ab80f5080f2d7de
Davit Burton <davitburton280@gmail.com>
2020-12-11 11:50:45 +0400

    New components for ferry routes management

---
+++ src/app/ferries/manage-table-routes/manage-table-routes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-table-routes',
+  templateUrl: './manage-table-routes.component.html',
+  styleUrls: ['./manage-table-routes.component.scss']
+})
+export class ManageTableRoutesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
