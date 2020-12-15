5624a1e021d9039ef43509a02a261c0d61c295a0
Grigor Sargsyan <sargr86@gmail.com>
2020-12-15 14:55:09 +0400

    Renamed config.json to config.js in backend, import routes via json file, fixed statistics on the ferry-map-routes page

--- src/app/ferries/ferries.module.ts
+++ src/app/ferries/ferries.module.ts
@@ -9,7 +9,7 @@ import {FerriesHomeComponent} from './ferries-home/ferries-home.component';
 import {FerriesHeaderComponent} from './ferries-header/ferries-header.component';
 import {ChatModule} from '@app/chat/chat.module';
 import {ManageAllComponent} from './manage-all/manage-all.component';
-import {ImportFromFileComponent} from './manage-routes/import-from-file/import-from-file.component';
+import {ImportFromFileComponent} from './manage-routes/routes-page-top/import-from-file/import-from-file.component';
 import {RoutesPricesTableComponent} from './manage-table-routes/routes-prices-table/routes-prices-table.component';
 import {ShowOrdersComponent} from '@app/ferries/show-orders/show-orders.component';
 import { ManageMapRoutesComponent } from './manage-routes/manage-map-routes/manage-map-routes.component';
