9d970c0191e03d2406e369f0e1cb8eeacc524893
Sofia Bruno <sofiabruno3003@gmail.com>
2020-12-15 14:55:09 +0400

    Renamed config.json to config.js in backend, import routes via json file, fixed statistics on the ferry-map-routes page

--- src/app/ferries/manage-routes/routes-page-top/routes-page-top.component.ts
+++ src/app/ferries/manage-routes/routes-page-top/routes-page-top.component.ts
@@ -1,7 +1,11 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {SubjectService} from '@core/services/subject.service';
 import {SaveRouteDialogComponent} from '@core/components/dialogs/save-route-dialog/save-route-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
+import {FerriesService} from '@core/services/ferries.service';
+import {ToastrService} from 'ngx-toastr';
+import * as XLSX from 'xlsx';
+import {ImportFromFileComponent} from '@app/ferries/manage-routes/routes-page-top/import-from-file/import-from-file.component';
 
 @Component({
   selector: 'app-routes-page-top',
@@ -13,9 +17,12 @@ export class RoutesPageTopComponent implements OnInit {
   routesWithNoPriceLen = 0;
   routesWithPricesLen = 0;
   routesOnMap = 0;
+  routesOnly;
+  @ViewChild(ImportFromFileComponent) fileImport: ImportFromFileComponent;
+
   constructor(
     private subject: SubjectService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
   ) {
     this.subject.getFerryRoutesData().subscribe(dt => {
       const routesOnly = dt.filter(d => !d.hasOwnProperty('single') && !d.hasOwnProperty('return'));
@@ -40,5 +47,21 @@ export class RoutesPageTopComponent implements OnInit {
       this.newRoute.emit();
     });
   }
+
+  openFileDialog(fileType) {
+    this.routesOnly = fileType !== 'json';
+    const input = <HTMLInputElement>document.getElementById('import-file');
+    input.accept = fileType === 'json' ? 'application/json' : 'application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    input.click();
+  }
+
+  fileSelected(file) {
+    if (this.routesOnly) {
+      this.fileImport.importXMLFile(file);
+    } else {
+      this.fileImport.importJSONFile(file);
+    }
+  }
+
 
 }
