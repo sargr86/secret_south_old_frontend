e522a461d0ac017f41841b5d767592fddadbc8c1
Sofia Bruno <sofiabruno3003@gmail.com>
2020-12-21 12:36:28 +0400

    Latest changes of ferry routes

--- src/app/tours/show-orders/show-orders.component.ts
+++ src/app/tours/show-orders/show-orders.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToursService} from '@core/services/tours.service';
+import {CommonService} from '@core/services/common.service';
 
 @Component({
   selector: 'app-show-orders',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-orders.component.scss']
 })
 export class ShowOrdersComponent implements OnInit {
+  tours;
 
-  constructor() { }
+  constructor(
+    private toursService: ToursService,
+    public common: CommonService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.tours = this.toursService.getAllTours();
+    this.common.dataLoading = false;
   }
 
 }
