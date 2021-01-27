b61061573800aa2a5222d2b37301ce89abe2b9f1
David Burton <davitburton280@gmail.com>
2021-01-27 17:47:29 +0400

    Searching daily tours from tours main page

--- src/app/tours/show-tours/show-tours.component.ts
+++ src/app/tours/show-tours/show-tours.component.ts
@@ -4,25 +4,28 @@ import {Observable} from 'rxjs/internal/Observable';
 import {ToursService} from '@core/services/tours.service';
 import {Tour} from '@shared/models/Tour';
 import {AuthService} from '@core/services/auth.service';
+import {CommonService} from '@core/services/common.service';
 
 @Component({
-    selector: 'app-show-tours',
-    templateUrl: './show-tours.component.html',
-    styleUrls: ['./show-tours.component.scss']
+  selector: 'app-show-tours',
+  templateUrl: './show-tours.component.html',
+  styleUrls: ['./show-tours.component.scss']
 })
 export class ShowToursComponent implements OnInit {
-    displayedColumns = TOURS_TABLE_COLUMNS;
-    spinnerDiameter = SPINNER_DIAMETER;
-    tours: Observable<Tour>;
+  displayedColumns = TOURS_TABLE_COLUMNS;
+  spinnerDiameter = SPINNER_DIAMETER;
+  tours: Observable<Tour>;
 
-    constructor(
-        private _tours: ToursService,
-        public auth: AuthService
-    ) {
-    }
+  constructor(
+    private _tours: ToursService,
+    public auth: AuthService,
+    public common: CommonService
+  ) {
+  }
 
-    ngOnInit() {
-        this.tours = this._tours.getAllTours();
-    }
+  ngOnInit() {
+    this.tours = this._tours.getAllTours();
+    this.common.dataLoading = false;
+  }
 
 }
--- src/app/tours/tours-list/tours-list.component.ts
+++ src/app/tours/tours-list/tours-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToursService} from '@core/services/tours.service';
+import {Router} from '@angular/router';
+import {TOURS_FOLDER} from '@core/constants/global';
 
 @Component({
   selector: 'app-tours-list',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tours-list.component.scss']
 })
 export class ToursListComponent implements OnInit {
+  tours = [];
+  toursFolder = TOURS_FOLDER;
 
-  constructor() { }
+  constructor(
+    private toursService: ToursService,
+    public router: Router
+  ) {
+  }
 
   ngOnInit(): void {
+    this.searchInDailyTours();
+  }
+
+  searchInDailyTours() {
+    const toursSearch = JSON.parse(localStorage.getItem('toursSearch'));
+    if (toursSearch) {
+      this.toursService.searchInDailyTours({search: toursSearch.name, date: toursSearch.date}).subscribe((dt: any) => {
+        this.tours = dt;
+      });
+    }
+  }
+
+  getPath(item, folder) {
+    const name = item.name.replace(/ /g, '_').replace(/&/g, '')
+    return folder + '/' + decodeURIComponent(name) + '/' + item.img;
   }
 
 }
