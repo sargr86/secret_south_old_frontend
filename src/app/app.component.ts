9dd41480a6436006208b2a4223e6fcbd8556ecf8
Sofia Bruno <sofiabruno3003@gmail.com>
2021-02-03 17:21:17 +0400

    Fixed selected cover star marking on ngx-gallery, added reset-cover, price value bug when editing a tour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -180,7 +180,6 @@ export class AppComponent implements OnInit, OnDestroy, AfterContentChecked {
   }
 
   get responsiveMode() {
-
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
